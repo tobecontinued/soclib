791c66704ad3cfc74f5223876fa0222ff300093f
becoulet <becoulet@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2007-09-13 15:08:40 +0000

    code cleanup, old comments removed
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@29 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- platforms/timer_4mips/segmentation.h
+++ platforms/timer_4mips/segmentation.h
@@ -17,18 +17,16 @@
 
 /////////////////////////////////////////////////////////////////
 //	text, reset, and exception segments
-//	
-//	Those three segments are mandatory
-//	The base adresses are imposed by the 
-//	MIPS processor architecture
 /////////////////////////////////////////////////////////////////
 
 #define	TEXT_BASE	0x00400000
 #define	TEXT_SIZE	0x00050000
 
+/* base address required by MIPS processor */
 #define	RESET_BASE	0xBFC00000
 #define	RESET_SIZE	0x00010000
 
+/* base address required by MIPS processor */
 #define	EXCEP_BASE	0x80000080
 #define	EXCEP_SIZE	0x00010000
 
@@ -41,9 +39,6 @@
 
 /////////////////////////////////////////////////////////
 //	local data segments 
-//	
-//	The number of local segments
-//	must be equal to the number of processors
 /////////////////////////////////////////////////////////
 
 #define	LOC0_BASE	0x20000000
@@ -59,38 +54,15 @@
 #define	LOC3_SIZE	0x00001000
 
 //////////////////////////////////////////////////////////
-//	System TTY and private TTYs
-//	
-//	- The system TTY is used by the printf() function.
-//	This segment must always be defined, 
-//	as it is used by the Mutek kernel. 
-//	- The private TTYs are used by the pprintf() function.
-//	The number of TTYs segments must be equal 
-//	to the number of processors.
+//	System devices
 ///////////////////////////////////////////////////////////
 
 #define	TTY_BASE	0xC0200000
 #define	TTY_SIZE	0x00000258
 
-/////////////////////////////////////////////////////////
-//	System Peripherals
-//
-//	Those segments must be defined for MUTEK,
-//	even if the corresponding peripherals
-//	are not instanciated in the hardware platform
-/////////////////////////////////////////////////////////
-
 #define	TIMER_BASE	0xB0200000
 #define	TIMER_SIZE	0x00000100
 
-#define	ITC_BASE	0xB1200000
-#define	ITC_SIZE	0x00001000
-
 #define	LOCKS_BASE	0xB2200000
 #define	LOCKS_SIZE	0x00001000
-
-#define	DFT_BASE	0xB3200000
-#define	DFT_SIZE	0x00001000
-
-
 
