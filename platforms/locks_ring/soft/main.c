7b1e7874eb6971d72a265d35876b0db79d72868b
yang <yang@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-01-25 12:40:03 +0000

    Ring interconnect platform added
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@181 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ platforms/locks_ring/soft/main.c
@@ -0,0 +1,55 @@
+/*
+ *
+ * SOCLIB_GPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU GPLv2.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ * 
+ * SOCLIB_GPL_HEADER_END
+ *
+ * Copyright (c) UPMC, Lip6, SoC
+ *         Nicolas Pouillon <nipo@ssji.net>, 2006-2007
+ *
+ * Maintainers: nipo
+ */
+
+#include "soclib/timer.h"
+#include "system.h"
+
+#include "../segmentation.h"
+
+extern uint32_t lock;
+
+int main(void)
+{
+	const int cpu = procnum();
+	int i;
+
+	printf("Cpu %x booted\n", cpu);
+
+	for (i=0; i<100; ++i) {
+		lock_lock(&lock);
+		printf("Hello from cpu %x\n", cpu);
+		lock_unlock(&lock);
+	}
+	if ( cpu ) {
+		while(1);
+	} else {
+		for (i=0; i<1000; ++i)
+			;
+		exit(0);
+	}
+}
