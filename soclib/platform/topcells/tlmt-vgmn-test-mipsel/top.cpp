7fdc4d3edfb1b45b94644737aa7274bb92d013e6
alinevieiramello@hotmail.com <alinevieiramello@hotmail.com@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-06-16 12:09:29 +0000

    Change of vci_xcache for vci_xcache_wrapper
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@389 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/platform/topcells/tlmt-vgmn-test-mipsel/top.cpp
+++ soclib/platform/topcells/tlmt-vgmn-test-mipsel/top.cpp
@@ -3,55 +3,82 @@
 #include <tlmt>
 #include "vci_ports.h"
 #include "mapping_table.h"
-#include "vci_vgmn.h"
-#include "vci_xcache.h"
-#include "vci_simple_target.h"
-#include "vci_multi_tty.h"
-#include "vci_ram.h"
-#include "segmentation.h"
+#include "iss.h"
 #include "mips.h"
+#include "vci_xcache_wrapper.h"
+#include "vci_vgmn.h"
+#include "vci_ram.h"
+#include "tty.h"
+#include "vci_multi_tty.h"
+
+#include <sys/timeb.h>
 
 int sc_main(int argc, char **argv)
 {
-	typedef soclib::tlmt::VciParams<uint32_t,uint32_t,4> vci_param;
+  struct timeb initial, final;
 
-	// Avoid repeating these everywhere
-	using soclib::common::IntTab;
-	using soclib::common::Segment;
+  ftime(&initial);
 
-	// Mapping table
+  typedef soclib::tlmt::VciParams<uint32_t,uint32_t,4> vci_param;
 
-	soclib::common::MappingTable maptab(32, IntTab(8), IntTab(8), 0x00300000);
 
-	maptab.add(Segment("reset", RESET_BASE, RESET_SIZE, IntTab(0), true));
-	maptab.add(Segment("excep", EXCEP_BASE, EXCEP_SIZE, IntTab(0), true));
-	maptab.add(Segment("text" , TEXT_BASE , TEXT_SIZE , IntTab(0), true));
+  // Configurator instanciateOnStack
+  soclib::common::ElfLoader loader("soft/bin.soft");
+  soclib::common::MappingTable mapping_table(32, soclib::common::IntTab(8), soclib::common::IntTab(8), 0x00200000);
+  
+  // Configurator configure
+  mapping_table.add(soclib::common::Segment("boot",  0xbfc00000,       2048, soclib::common::IntTab(1), 1));
+  mapping_table.add(soclib::common::Segment("cram0", 0x10000000, 0x00100000, soclib::common::IntTab(0), 1));
+  mapping_table.add(soclib::common::Segment("cram1", 0x20000000, 0x00100000, soclib::common::IntTab(1), 1));
+  mapping_table.add(soclib::common::Segment("excep", 0x80000080,       2048, soclib::common::IntTab(1), 1));
+  mapping_table.add(soclib::common::Segment("tty0",  0x90200000,         32, soclib::common::IntTab(2), 0));
+  mapping_table.add(soclib::common::Segment("uram0", 0x10200000, 0x00100000, soclib::common::IntTab(0), 0));
+  mapping_table.add(soclib::common::Segment("uram1", 0x20200000, 0x00100000, soclib::common::IntTab(1), 0));
+  
+  /////////////////////////////////////////////////////////////////////////////
+  // VCI_VGMN 
+  /////////////////////////////////////////////////////////////////////////////
+  soclib::tlmt::VciVgmn<vci_param> vgmn(1,3,mapping_table,3);
 
-	maptab.add(Segment("data" , DATA_BASE , DATA_SIZE , IntTab(0), true));
+  /////////////////////////////////////////////////////////////////////////////
+  // VCI_XCACHE 
+  /////////////////////////////////////////////////////////////////////////////
+  soclib::tlmt::VciXcacheWrapper<soclib::common::MipsElIss,vci_param> mips0("mips0", 0, mapping_table, 64, 8, 1, 8);
 
-	maptab.add(Segment("loc0" , LOC0_BASE , LOC0_SIZE , IntTab(0), true));
+  mips0.p_vci(vgmn.m_RspArbCmdRout[0]->p_vci);
 
-	maptab.add(Segment("tty"  , TTY_BASE  , TTY_SIZE  , IntTab(1), false));
+  /////////////////////////////////////////////////////////////////////////////
+  // RAM
+  /////////////////////////////////////////////////////////////////////////////
+  soclib::tlmt::VciRam<vci_param> ram0("ram0", 0, soclib::common::IntTab(0), mapping_table, loader);
 
-	soclib::tlmt::VciVgmn<vci_param> vgmn(1,2,maptab,10);
+  vgmn.m_CmdArbRspRout[0]->p_vci(ram0.p_vci);
 
-	soclib::tlmt::VciXcache<soclib::common::MipsElIss,vci_param> i("init",0);
-	//soclib::tlmt::VciSimpleTarget<vci_param> t("target");
+  /////////////////////////////////////////////////////////////////////////////
+  // RAM
+  /////////////////////////////////////////////////////////////////////////////
+  soclib::tlmt::VciRam<vci_param> ram1("ram1", 1, soclib::common::IntTab(1), mapping_table, loader);
 
-	soclib::common::ElfLoader loader("soft/bin.soft");
-	soclib::tlmt::VciRam<vci_param> ram("ram", 0, IntTab(0), maptab, loader);
-	soclib::tlmt::VciMultiTty<vci_param> tty("tty", IntTab(1), maptab, "tty0", NULL);
+  vgmn.m_CmdArbRspRout[1]->p_vci(ram1.p_vci);
 
-	std::cout << "ram initialisee" << std::endl;
+  /////////////////////////////////////////////////////////////////////////////
+  // TTY
+  /////////////////////////////////////////////////////////////////////////////
+  soclib::tlmt::VciMultiTty<vci_param> vcitty("tty0",soclib::common::IntTab(2), mapping_table, "TTY0", NULL);
 
-	//i.p_vci(t.p_vci);
-	i.p_vci(vgmn.m_RspArbCmdRout[0]->p_vci);
-	vgmn.m_CmdArbRspRout[0]->p_vci(ram.p_vci);
-	vgmn.m_CmdArbRspRout[1]->p_vci(tty.p_vci);
+  vgmn.m_CmdArbRspRout[2]->p_vci(vcitty.p_vci);
+  (*vcitty.p_irq[0])(mips0.p_irq[0]);
+ 
 
-	tty.p_out(i.p_irq[2]);
+  /////////////////////////////////////////////////////////////////////////////
+  // START
+  /////////////////////////////////////////////////////////////////////////////
+  sc_core::sc_start();
 
-	sc_core::sc_start();
-	return 0;
+  ftime(&final);
+
+  std::cout << "Execution Time = " << (int)((1000.0 * (final.time - initial.time))+ (final.millitm - initial.millitm)) << std::endl;
+
+  return 0;
 }
 
