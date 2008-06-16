2f8fb46bb2479ccbac0874e00d96a6c87edd2a2e
alinevieiramello@hotmail.com <alinevieiramello@hotmail.com@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-06-16 11:27:46 +0000

    Add the vci_xcache_wrapper decribed in TLM-T model
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@386 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/module/internal_component/vci_xcache/tlmt/source/include/generic_cache.h
+++ soclib/module/internal_component/vci_xcache/tlmt/source/include/generic_cache.h
@@ -6,6 +6,11 @@
 
 namespace soclib { namespace tlmt {
 
+#define LINE_VALID 0x80000000
+
+using soclib::common::uint32_log2;
+
+
 template<typename vci_param>
 class genericCache 
 {
@@ -13,7 +18,6 @@ class genericCache
 private:
 	typename vci_param::data_t**   m_data ;
 	typename vci_param::addr_t*    m_tag ;
-	bool*                          m_val ;
 	soclib::common::AddressMaskingTable<uint32_t>  m_x ;
 	soclib::common::AddressMaskingTable<uint32_t>  m_y ;
 	soclib::common::AddressMaskingTable<uint32_t>  m_z ;
@@ -22,34 +26,34 @@ private:
 	uint32_t m_yzmask;	
 
 public:
+
 ///////// Constructor ////////////////
-genericCache( uint32_t nlines, uint32_t nwords ) :
-    m_x( soclib::common::uint32_log2(nwords), 2 ),
-    m_y( soclib::common::uint32_log2(nlines), 2+soclib::common::uint32_log2(nwords) ),
-    m_z( 32-soclib::common::uint32_log2(nwords)-soclib::common::uint32_log2(nlines)-2, 
-    soclib::common::uint32_log2(nwords)+soclib::common::uint32_log2(nlines)+2 ),
-    m_yzmask((~0)<<(soclib::common::uint32_log2(nwords) + soclib::common::uint32_log2(4)))
+ genericCache( uint32_t nlines, uint32_t nwords ):
+	m_x( uint32_log2(nwords), uint32_log2(vci_param::nbytes)),
+	  m_y( uint32_log2(nlines), uint32_log2(nwords) + uint32_log2(vci_param::nbytes)),
+	  m_z((vci_param::nbytes * 8) - uint32_log2(nwords)-uint32_log2(nlines)-uint32_log2(vci_param::nbytes),
+	      uint32_log2(nwords)+uint32_log2(nlines)+uint32_log2(vci_param::nbytes)),
+	  m_yzmask((~0)<<(uint32_log2(nwords) + uint32_log2(vci_param::nbytes)))
 {
-    m_data = new typename vci_param::data_t*[nlines] ;
-    for ( uint32_t i = 0 ; i < nlines ; i++ ) m_data[i] = new typename vci_param::data_t[nwords] ;
-    m_tag = new typename vci_param::addr_t[nlines] ;
-    m_val = new bool[nlines] ;
-    m_words = nwords ;
-    m_lines = nlines ;
+  m_data = new typename vci_param::data_t*[nlines] ;
+  for ( uint32_t i = 0 ; i < nlines ; i++ ) m_data[i] = new typename vci_param::data_t[nwords] ;
+  m_tag = new typename vci_param::addr_t[nlines] ;
+  m_words = nwords ;
+  m_lines = nlines ;
 }
 
 //////////////////////////////
 inline void reset( )
     {
-    for ( size_t i = 0 ; i < m_lines ; i++ ) m_val[i] = false ;
+      for ( size_t i = 0 ; i < m_lines ; i++ ) m_tag[i] = 0;
     }
 
 //////////////////////////////
 inline bool miss(typename vci_param::addr_t ad )
     {
     uint32_t y = m_y[ad] ;
-    uint32_t z = m_z[ad] ;
-    return !( ( m_tag[y] == z ) && m_val[y] ) ;
+    uint32_t z = m_z[ad] | LINE_VALID;
+    return !( m_tag[y] == z );
     }
 
 ///////////////////////////////
@@ -63,7 +67,6 @@ inline typename vci_param::data_t read(typename vci_param:: addr_t ad )
 /////////////////////////////////////////
 inline void write( typename vci_param::addr_t ad, typename vci_param::data_t dt )
     {
-// comment fait-on si l'écriture est sur un half ou un byte ?
     uint32_t y = m_y[ad] ;
     uint32_t x = m_x[ad] ;
     m_data[y][x] = dt ;
@@ -71,18 +74,17 @@ inline void write( typename vci_param::addr_t ad, typename vci_param::data_t dt 
 
 //////////////////////////////
 inline void inval( typename vci_param::addr_t ad )
-    {
+{
     uint32_t y = m_y[ad] ;
-    m_val[y] = false ;
-    }
+    m_tag[y] = 0;
+}
 
 //////////////////////////////
 inline void update( typename vci_param::addr_t ad, typename vci_param::data_t *buf )
     {
     uint32_t y = m_y[ad] ;
-    uint32_t z = m_z[ad] ;
-    m_val[y] = true ;
-    m_tag[y] = z ;
+    uint32_t z = m_z[ad] | LINE_VALID;
+    m_tag[y] = z;
     for ( uint32_t i = 0 ; i < m_words ; i++ ) m_data[y][i] = buf[i] ;
     }
 
