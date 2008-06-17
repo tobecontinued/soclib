72c8036c320b25cafd76b8d2b19d5fbfcd89f9ae
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-06-17 09:57:04 +0000

    Add arm compiler flags defaults
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@395 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- utils/conf/soft_flags.mk
+++ utils/conf/soft_flags.mk
@@ -1,14 +1,25 @@
-mipsel_CFLAGS=-mips32 -mno-branch-likely -gstabs+
+mipseb_CC_PREFIX=mips-unknown-elf-
 mipseb_CFLAGS=-mips32 -mno-branch-likely -gstabs+
-powerpc_CFLAGS=-mcpu=405 -mstrict-align -gstabs+
-microblaze_CFLAGS=-mno-xl-soft-div -mno-xl-soft-mul -gstabs+
-microblaze_LDFLAGS=-nostdlib
-nios2_CFLAGS=-mhw-mul -mhw-div
 
 mipsel_CC_PREFIX=mipsel-unknown-elf-
-mipseb_CC_PREFIX=mips-unknown-elf-
+mipsel_CFLAGS=-mips32 -mno-branch-likely -gstabs+
+
 powerpc_CC_PREFIX=powerpc-unknown-elf-
+powerpc_CFLAGS=-mcpu=405 -mstrict-align -gstabs+
+
 microblaze_CC_PREFIX=mb-
+microblaze_CFLAGS=-mno-xl-soft-div -mno-xl-soft-mul -gstabs+
+microblaze_LDFLAGS=-nostdlib
+
 nios2_CC_PREFIX=nios2-unknown-elf-
+nios2_CFLAGS=-mhw-mul -mhw-div
+
+arm7tdmi_CC_PREFIX = armv5b-softfloat-linux-
+arm7tdmi_CFLAGS = -nostdinc -gstabs+
+arm7tdmi_LDFLAGS = -nostdlib
+
+arm966_CC_PREFIX = armv5b-softfloat-linux-
+arm966_CFLAGS = -nostdinc -gstabs+
+arm966_LDFLAGS = -nostdlib
 
 -include $(HOME)/.soclib/soft_compilers.conf
