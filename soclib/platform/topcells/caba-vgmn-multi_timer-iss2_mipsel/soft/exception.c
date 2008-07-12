2e400bf17a7bb32f3c0602ac478e535018e6ae0a
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-07-12 00:04:32 +0000

    Port some examples to caba xcache_wrapper and ississ2 wrapper
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@428 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ soclib/platform/topcells/caba-vgmn-multi_timer-iss2_mipsel/soft/exception.c
@@ -0,0 +1,192 @@
+/*
+    This file is part of MutekH.
+
+    MutekH is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    MutekH is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MutekH; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+    Copyright Alexandre Becoulet <alexandre.becoulet@lip6.fr> (c) 2006
+
+*/
+
+asm(
+    ".section        .excep,\"ax\",@progbits			\n"
+
+    ".globl mips_interrupt_entry				\n"
+    "mips_interrupt_entry:					\n"
+    ".set push							\n"
+    ".set noat							\n"
+
+    /* save registers */
+    "	addu	$sp,	-4*32				\n"
+
+    "	sw	$1,	 1*4($sp)		        \n" /* AT reg */
+
+    "	sw	$2,	 2*4($sp)		        \n" /* Return value regs */
+    "	sw	$3,	 3*4($sp)		        \n" /* Return value regs */
+
+    "	mfc0	$1,	$13				\n" /* read Cause */
+
+    "	sw	$4,	 4*4($sp)		        \n" /* Args regs */
+    "	sw	$5,	 5*4($sp)		        \n" /* Args regs */
+    "	sw	$6,	 6*4($sp)		        \n" /* Args regs */
+    "	sw	$7,	 7*4($sp)		        \n" /* Args regs */
+
+    "	sw	$8,	 8*4($sp)		        \n" /* Temp regs */
+    "	sw	$9,	 9*4($sp)		        \n" /* Temp regs */
+    "	sw	$10,	10*4($sp)		        \n" /* Temp regs */
+    "	sw	$11,	11*4($sp)		        \n" /* Temp regs */
+
+    "	mfc0	$8,	$14				\n" /* read EPC */
+
+    "	sw	$12,	12*4($sp)		        \n" /* Temp regs */
+    "	sw	$13,	13*4($sp)		        \n" /* Temp regs */
+    "	sw	$14,	14*4($sp)		        \n" /* Temp regs */
+    "	sw	$15,	15*4($sp)		        \n" /* Temp regs */
+    "	sw	$24,	24*4($sp)		        \n" /* Temp regs */
+    "	sw	$25,	25*4($sp)		        \n" /* Temp regs */
+
+    "	sw	$31,	31*4($sp)		        \n" /* Return address regs */
+
+    "	sw	$8,	0*4($sp)		        \n" /* EPC reg */
+
+    "	andi	$9,	$1,	0x3c			\n" /* extract cause */
+    "	beq	$9,	$0,	interrupt_hw		\n"
+    "	li	$10,	(8<<2)				\n"
+    "	beq	$9,	$10,	interrupt_sys		\n"
+
+    /*************************************************************
+		exception handling
+    **************************************************************/
+
+    "interrupt_ex:					\n"
+
+    /* add missing registers in reg table */
+    "	sw	$16,	16*4($sp)		        \n"
+    "	sw	$17,	17*4($sp)		        \n"
+    "	sw	$18,	18*4($sp)		        \n"
+    "	sw	$19,	19*4($sp)		        \n"
+    "	sw	$20,	20*4($sp)		        \n"
+    "	sw	$21,	21*4($sp)		        \n"
+    "	sw	$22,	22*4($sp)		        \n"
+    "	sw	$23,	23*4($sp)		        \n"
+    "	sw	$26,	26*4($sp)		        \n"
+    "	sw	$27,	27*4($sp)		        \n"
+    "	sw	$28,	28*4($sp)		        \n"
+    "	sw	$29,	29*4($sp)		        \n"
+    "	sw	$30,	30*4($sp)		        \n"
+
+    /* exception function arguments */
+    "	srl	$4,	$9,	2			\n" /* adjust cause arg */
+    "	move	$5,	$8				\n" /* execution pointer */
+    "	mfc0	$6,	$8				\n" /* bad address if any */
+    "	addiu	$7,	$sp,	0			\n" /* register table on stack */
+    "	addiu	$8,	$sp,				\n" /* stack pointer */
+
+    "	sw	$8,	4*4($sp)			\n"
+    "	la		$1,	interrupt_ex_handler	\n"
+    "	jalr	$1					\n"
+
+    "	j	return					\n"
+
+    /*************************************************************
+		syscall handling
+    **************************************************************/
+    "interrupt_sys:					\n"
+
+    "	la		$1,	interrupt_sys_handler	\n"
+    "	jalr	$1					\n"
+
+    "	j	return_val				\n"
+
+    /*************************************************************
+		hardware interrupts handling
+    **************************************************************/
+    "interrupt_hw:					\n"
+
+    "	srl	$4,	$1,	8			\n"
+    "	andi	$4,	$4,	0xff			\n"
+
+    "	la		$1,	interrupt_hw_handler	\n"
+    "	jalr	$1					\n"
+
+    /************************************************************/
+
+    /* restore registers */
+    "return:						\n"
+    "	lw	$2,	 2*4($sp)		        \n"
+    "	lw	$3,	 3*4($sp)		        \n"
+
+    "return_val:					\n"
+    "	lw	$1,	 1*4($sp)		        \n"
+
+    "	lw	$26,	 0*4($sp)		        \n" /* EPC reg */
+
+    "	lw	$4,	 4*4($sp)		        \n"
+    "	lw	$5,	 5*4($sp)		        \n"
+    "	lw	$6,	 6*4($sp)		        \n"
+    "	lw	$7,	 7*4($sp)		        \n"
+
+    "	lw	$8,	 8*4($sp)		        \n"
+    "	lw	$9,	 9*4($sp)		        \n"
+    "	lw	$10,	10*4($sp)		        \n"
+    "	lw	$11,	11*4($sp)		        \n"
+    "	lw	$12,	12*4($sp)		        \n"
+    "	lw	$13,	13*4($sp)		        \n"
+    "	lw	$14,	14*4($sp)		        \n"
+    "	lw	$15,	15*4($sp)		        \n"
+
+    "	lw	$24,	24*4($sp)		        \n"
+    "	lw	$25,	25*4($sp)		        \n"
+
+    "	lw	$31,	31*4($sp)		        \n"
+
+    "	addu	$sp,	4*32				\n"
+
+    ".set noreorder					\n"
+    "	jr	$26					\n"
+    "	rfe						\n"
+
+    ".set pop						\n"
+    );
+
+asm(
+    ".section        .reset,\"ax\",@progbits		\n"
+
+    ".globl cpu_boot					\n"
+    "cpu_boot:						\n"
+
+    ".set push						\n"
+    ".set noreorder					\n"
+
+    "li        $8,   0x0000FF15  			\n"
+    "mtc0      $8,	$12         			\n"
+
+    /* get CPU id and adjust stack */
+
+    "mfc0	$8,	$15, 1				\n"
+    "la         $sp,	_stack - 16	\n"
+	"andi   $8, $8, 0x3ff		\n"
+    "sll	$8,	$8,	10		\n"
+    "subu	$sp,	$sp,	$8		\n"
+
+    /* setup global data pointer */
+    "la	   $gp,   _gp					\n"
+
+    "la         $8,   main				\n"
+    "j          $8					\n"
+    "nop						\n"
+    "							\n"
+    ".set pop						\n"
+    );
+
--- soclib/platform/topcells/caba-vgmn-multi_timer-iss2_mipsel/top.cpp
+++ soclib/platform/topcells/caba-vgmn-multi_timer-iss2_mipsel/top.cpp
@@ -33,12 +33,15 @@
 #include "mips.h"
 #include "ississ2.h"
 #include "vci_xcache_wrapper.h"
+#include "vci_timer.h"
 #include "vci_ram.h"
 #include "vci_multi_tty.h"
-#include "vci_simhelper.h"
+#include "vci_locks.h"
 #include "vci_vgmn.h"
 
 #include "segmentation.h"
+
+//soclib::common::IntTab a(8);
 
 int _main(int argc, char *argv[])
 {
@@ -51,16 +54,23 @@ int _main(int argc, char *argv[])
 	typedef soclib::caba::VciParams<4,1,32,1,1,1,8,1,1,1> vci_param;
 
 	// Mapping table
+
 	soclib::common::MappingTable maptab(32, IntTab(8), IntTab(8), 0x00300000);
 
 	maptab.add(Segment("reset", RESET_BASE, RESET_SIZE, IntTab(0), true));
 	maptab.add(Segment("excep", EXCEP_BASE, EXCEP_SIZE, IntTab(0), true));
 	maptab.add(Segment("text" , TEXT_BASE , TEXT_SIZE , IntTab(0), true));
-	maptab.add(Segment("data" , DATA_BASE , DATA_SIZE , IntTab(0), true));
   
-	maptab.add(Segment("tty"  , TTY_BASE  , TTY_SIZE  , IntTab(1), false));
-
-	maptab.add(Segment("simhelper"  , SIMHELPER_BASE  , SIMHELPER_SIZE  , IntTab(2), false));
+	maptab.add(Segment("data" , DATA_BASE , DATA_SIZE , IntTab(1), true));
+  
+	maptab.add(Segment("loc0" , LOC0_BASE , LOC0_SIZE , IntTab(1), true));
+	maptab.add(Segment("loc1" , LOC1_BASE , LOC1_SIZE , IntTab(1), true));
+	maptab.add(Segment("loc2" , LOC2_BASE , LOC2_SIZE , IntTab(1), true));
+	maptab.add(Segment("loc3" , LOC3_BASE , LOC3_SIZE , IntTab(1), true));
+  
+	maptab.add(Segment("tty"  , TTY_BASE  , TTY_SIZE  , IntTab(2), false));
+	maptab.add(Segment("timer", TIMER_BASE, TIMER_SIZE, IntTab(3), false));
+	maptab.add(Segment("locks", LOCKS_BASE, LOCKS_SIZE, IntTab(4), false));
 
 	// Signals
 
@@ -101,82 +111,107 @@ int _main(int argc, char *argv[])
 	soclib::caba::VciSignals<vci_param> signal_vci_m3("signal_vci_m3");
 
 	soclib::caba::VciSignals<vci_param> signal_vci_tty("signal_vci_tty");
-	soclib::caba::VciSignals<vci_param> signal_vci_simhelper("signal_vci_simhelper");
 	soclib::caba::VciSignals<vci_param> signal_vci_vcimultiram0("signal_vci_vcimultiram0");
+	soclib::caba::VciSignals<vci_param> signal_vci_vcitimer("signal_vci_vcitimer");
+	soclib::caba::VciSignals<vci_param> signal_vci_vcilocks("signal_vci_vcilocks");
+	soclib::caba::VciSignals<vci_param> signal_vci_vcimultiram1("signal_vci_vcimultiram1");
 
 	sc_signal<bool> signal_tty_irq0("signal_tty_irq0"); 
+	sc_signal<bool> signal_tty_irq1("signal_tty_irq1"); 
+	sc_signal<bool> signal_tty_irq2("signal_tty_irq2"); 
+	sc_signal<bool> signal_tty_irq3("signal_tty_irq3"); 
 
 	// Components
 
-	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > mips0("mips0", 0, maptab,IntTab(0), 1,8,4, 1,8,4);
-	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > mips1("mips1", 1, maptab,IntTab(1), 1,8,4, 1,8,4);
-	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > mips2("mips2", 2, maptab,IntTab(2), 1,8,4, 1,8,4);
-	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > mips3("mips3", 3, maptab,IntTab(3), 1,8,4, 1,8,4);
+	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > cache0("cache0", 0, maptab,IntTab(0), 1,8,4, 1,8,4);
+	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > cache1("cache1", 1, maptab,IntTab(1), 1,8,4, 1,8,4);
+	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > cache2("cache2", 2, maptab,IntTab(2), 1,8,4, 1,8,4);
+	soclib::caba::VciXcacheWrapper<vci_param, soclib::common::IssIss2<soclib::common::MipsElIss> > cache3("cache3", 3, maptab,IntTab(3), 1,8,4, 1,8,4);
 
 	soclib::common::ElfLoader loader("soft/bin.soft");
 	soclib::caba::VciMultiRam<vci_param> vcimultiram0("vcimultiram0", IntTab(0), maptab, loader);
-	soclib::caba::VciMultiTty<vci_param> vcitty("vcitty",	IntTab(1), maptab, "vcitty0", NULL);
-	soclib::caba::VciSimhelper<vci_param> vcisimhelper("vcisimhelper",	IntTab(2), maptab);
+	soclib::caba::VciMultiRam<vci_param> vcimultiram1("vcimultiram1", IntTab(1), maptab, loader);
+	soclib::caba::VciMultiTty<vci_param> vcitty("vcitty",	IntTab(2), maptab, "vcitty0", "vcitty1", "vcitty2", "vcitty3", NULL);
+	soclib::caba::VciTimer<vci_param> vcitimer("vcittimer", IntTab(3), maptab, 4);
+	soclib::caba::VciLocks<vci_param> vcilocks("vcilocks", IntTab(4), maptab); 
 	
-	soclib::caba::VciVgmn<vci_param> vgmn("vgmn",maptab, 4, 3, 2, 8);
+	soclib::caba::VciVgmn<vci_param> vgmn("vgmn",maptab, 4, 5, 2, 8);
 
 	//	Net-List
  
-	mips0.p_clk(signal_clk);  
-	mips1.p_clk(signal_clk);  
-	mips2.p_clk(signal_clk);  
-	mips3.p_clk(signal_clk);  
+	cache0.p_clk(signal_clk);
+	cache1.p_clk(signal_clk);
+	cache2.p_clk(signal_clk);
+	cache3.p_clk(signal_clk);
 	vcimultiram0.p_clk(signal_clk);
+	vcimultiram1.p_clk(signal_clk);
+	vcilocks.p_clk(signal_clk);
+	vcitimer.p_clk(signal_clk);
   
-	mips0.p_resetn(signal_resetn);  
-	mips1.p_resetn(signal_resetn);  
-	mips2.p_resetn(signal_resetn);  
-	mips3.p_resetn(signal_resetn);  
+	cache0.p_resetn(signal_resetn);
+	cache1.p_resetn(signal_resetn);
+	cache2.p_resetn(signal_resetn);
+	cache3.p_resetn(signal_resetn);
 	vcimultiram0.p_resetn(signal_resetn);
+	vcimultiram1.p_resetn(signal_resetn);
+	vcilocks.p_resetn(signal_resetn);
+	vcitimer.p_resetn(signal_resetn);
   
-	mips0.p_irq[0](signal_mips0_it0); 
-	mips0.p_irq[1](signal_mips0_it1); 
-	mips0.p_irq[2](signal_mips0_it2); 
-	mips0.p_irq[3](signal_mips0_it3); 
-	mips0.p_irq[4](signal_mips0_it4); 
-	mips0.p_irq[5](signal_mips0_it5); 
+	cache0.p_irq[0](signal_mips0_it0); 
+	cache0.p_irq[1](signal_mips0_it1); 
+	cache0.p_irq[2](signal_mips0_it2); 
+	cache0.p_irq[3](signal_mips0_it3); 
+	cache0.p_irq[4](signal_mips0_it4); 
+	cache0.p_irq[5](signal_mips0_it5); 
   
-	mips1.p_irq[0](signal_mips1_it0); 
-	mips1.p_irq[1](signal_mips1_it1); 
-	mips1.p_irq[2](signal_mips1_it2); 
-	mips1.p_irq[3](signal_mips1_it3); 
-	mips1.p_irq[4](signal_mips1_it4); 
-	mips1.p_irq[5](signal_mips1_it5); 
+	cache1.p_irq[0](signal_mips1_it0); 
+	cache1.p_irq[1](signal_mips1_it1); 
+	cache1.p_irq[2](signal_mips1_it2); 
+	cache1.p_irq[3](signal_mips1_it3); 
+	cache1.p_irq[4](signal_mips1_it4); 
+	cache1.p_irq[5](signal_mips1_it5); 
   
-	mips2.p_irq[0](signal_mips2_it0); 
-	mips2.p_irq[1](signal_mips2_it1); 
-	mips2.p_irq[2](signal_mips2_it2); 
-	mips2.p_irq[3](signal_mips2_it3); 
-	mips2.p_irq[4](signal_mips2_it4); 
-	mips2.p_irq[5](signal_mips2_it5); 
+	cache2.p_irq[0](signal_mips2_it0); 
+	cache2.p_irq[1](signal_mips2_it1); 
+	cache2.p_irq[2](signal_mips2_it2); 
+	cache2.p_irq[3](signal_mips2_it3); 
+	cache2.p_irq[4](signal_mips2_it4); 
+	cache2.p_irq[5](signal_mips2_it5); 
   
-	mips3.p_irq[0](signal_mips3_it0); 
-	mips3.p_irq[1](signal_mips3_it1); 
-	mips3.p_irq[2](signal_mips3_it2); 
-	mips3.p_irq[3](signal_mips3_it3); 
-	mips3.p_irq[4](signal_mips3_it4); 
-	mips3.p_irq[5](signal_mips3_it5); 
+	cache3.p_irq[0](signal_mips3_it0); 
+	cache3.p_irq[1](signal_mips3_it1); 
+	cache3.p_irq[2](signal_mips3_it2); 
+	cache3.p_irq[3](signal_mips3_it3); 
+	cache3.p_irq[4](signal_mips3_it4); 
+	cache3.p_irq[5](signal_mips3_it5); 
         
-	mips0.p_vci(signal_vci_m0);
-	mips1.p_vci(signal_vci_m1);
-	mips2.p_vci(signal_vci_m2);
-	mips3.p_vci(signal_vci_m3);
+	cache0.p_vci(signal_vci_m0);
+
+	cache1.p_vci(signal_vci_m1);
+
+	cache2.p_vci(signal_vci_m2);
+
+	cache3.p_vci(signal_vci_m3);
 
 	vcimultiram0.p_vci(signal_vci_vcimultiram0);
+
+	vcitimer.p_vci(signal_vci_vcitimer);
+	vcitimer.p_irq[0](signal_mips0_it0); 
+	vcitimer.p_irq[1](signal_mips1_it0); 
+	vcitimer.p_irq[2](signal_mips2_it0); 
+	vcitimer.p_irq[3](signal_mips3_it0); 
+  
+	vcilocks.p_vci(signal_vci_vcilocks);
+  
+	vcimultiram1.p_vci(signal_vci_vcimultiram1);
 
 	vcitty.p_clk(signal_clk);
 	vcitty.p_resetn(signal_resetn);
 	vcitty.p_vci(signal_vci_tty);
 	vcitty.p_irq[0](signal_tty_irq0); 
-
-	vcisimhelper.p_clk(signal_clk);
-	vcisimhelper.p_resetn(signal_resetn);
-	vcisimhelper.p_vci(signal_vci_simhelper);
+	vcitty.p_irq[1](signal_tty_irq1); 
+	vcitty.p_irq[2](signal_tty_irq2); 
+	vcitty.p_irq[3](signal_tty_irq3); 
 
 	vgmn.p_clk(signal_clk);
 	vgmn.p_resetn(signal_resetn);
@@ -187,15 +222,46 @@ int _main(int argc, char *argv[])
 	vgmn.p_to_initiator[3](signal_vci_m3);
 
 	vgmn.p_to_target[0](signal_vci_vcimultiram0);
-	vgmn.p_to_target[1](signal_vci_tty);
-	vgmn.p_to_target[2](signal_vci_simhelper);
+	vgmn.p_to_target[1](signal_vci_vcimultiram1);
+	vgmn.p_to_target[2](signal_vci_tty);
+	vgmn.p_to_target[3](signal_vci_vcitimer);
+	vgmn.p_to_target[4](signal_vci_vcilocks);
 
+
+	int ncycles;
+#ifndef SOCVIEW
+	if (argc == 2) {
+		ncycles = std::atoi(argv[1]);
+	} else {
+		std::cerr
+			<< std::endl
+			<< "The number of simulation cycles must "
+			   "be defined in the command line"
+			<< std::endl;
+		exit(1);
+	}
+
+	sc_start(sc_core::sc_time(0, SC_NS));
+	signal_resetn = false;
+
+	sc_start(sc_core::sc_time(1, SC_NS));
+	signal_resetn = true;
+
+	for (int i = 0; i < ncycles ; i+=10000) {
+		sc_start(sc_core::sc_time(100000, SC_NS));
+		std::cout << "Time elapsed: "<<i<<" cycles." << std::endl;
+	}
+	return EXIT_SUCCESS;
+#else
+	ncycles = 1;
 	sc_start(sc_core::sc_time(0, SC_NS));
 	signal_resetn = false;
 	sc_start(sc_core::sc_time(1, SC_NS));
 	signal_resetn = true;
-	sc_start();
+
+	debug();
 	return EXIT_SUCCESS;
+#endif
 }
 
 int sc_main (int argc, char *argv[])
