f4e231edd92d2416398b5fce585189bba31f2879
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-07-30 15:32:32 +0000

    Update mips compiler definition
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@481 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- utils/conf/soft_flags.mk
+++ utils/conf/soft_flags.mk
@@ -1,14 +1,18 @@
-mipseb_CC_PREFIX=mips-unknown-elf-
-mipseb_CFLAGS=-mips32 -mno-branch-likely -gstabs+ -DSOCLIB_MIPS_R3000
+mipseb_CC_PREFIX=mipsel-unknown-elf-
+mipseb_CFLAGS=-mips2 -mno-branch-likely -gstabs+ -DSOCLIB_MIPS_R3000 -EB
+mipseb_LDFLAGS=-EB
 
 mipsel_CC_PREFIX=mipsel-unknown-elf-
-mipsel_CFLAGS=-mips32 -mno-branch-likely -gstabs+ -DSOCLIB_MIPS_R3000
+mipsel_CFLAGS=-mips2 -mno-branch-likely -gstabs+ -DSOCLIB_MIPS_R3000 -EL
+mipsel_LDFLAGS=-EL
 
-mips32eb_CC_PREFIX=mips-unknown-elf-
-mips32eb_CFLAGS=-mips32r2 -gstabs+ -DSOCLIB_MIPS32 
+mips32eb_CC_PREFIX=mipsel-unknown-elf-
+mips32eb_CFLAGS=-mips32r2 -gstabs+ -DSOCLIB_MIPS32 -EB
+mips32eb_LDFLAGS=-EB
 
 mips32el_CC_PREFIX=mipsel-unknown-elf-
-mips32el_CFLAGS=-mips32r2 -gstabs+ -DSOCLIB_MIPS32
+mips32el_CFLAGS=-mips32r2 -gstabs+ -DSOCLIB_MIPS32 -EL
+mips32el_LDFLAGS=-EL
 
 powerpc_CC_PREFIX=powerpc-unknown-elf-
 powerpc_CFLAGS=-mcpu=405 -mstrict-align -gstabs+
