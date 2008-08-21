2637b5018384833e4b1f2ca4d64affadd4ee41cb
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2008-08-21 11:14:44 +0000

    Add an exception address test
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@522 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ soclib/platform/runtime_netlist/cpu_test/mips_exception_address/system.c
@@ -0,0 +1,55 @@
+/*
+ *
+ * SOCLIB_GPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU GPLv2.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ * 
+ * SOCLIB_GPL_HEADER_END
+ *
+ * Copyright (c) UPMC, Lip6, SoC
+ *         Nicolas Pouillon <nipo@ssji.net>, 2006-2007
+ *
+ * Maintainers: nipo
+ */
+
+#include "system.h"
+
+extern void *bug_addr;
+
+void interrupt_ex_handler(
+	unsigned int type, void *execptr,
+	void *dataptr, void *regtable,
+	void *stackptr)
+{
+	exit(0);
+
+	int i;
+	printf("Exception at %p, hoping we except from %p\n", execptr, bug_addr);
+	for ( i=0; i<1000; i++ )
+		asm volatile("nop");
+	exit(bug_addr == execptr ? 0 : 2);
+}
+
+void interrupt_sys_handler(unsigned int irq)
+{
+	exit(3);
+}
+
+void interrupt_hw_handler(unsigned int irq)
+{
+	exit(4);
+}
