cb776d6f040db370421c26498531c52241be2592
tarik.graba@telecom-paristech.fr <tarik.graba@telecom-paristech.fr@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-03-03 19:08:33 +0000

    LM32 (lattice mico32) iss2. LM32 instruction set simulator for SocLib This iss respects the iss2 interface.
    The LM32 is an opensource soft core CPU from lattice semi.
    It is a simple risc processor with 32 registers, less than 
    64 instructions and an optional data/instruction cache.
    It does not have MMU.
    
    more info, the verilog sources and the gcc toolchain can be 
    found here:
    http://www.latticesemi.com/dynamic/index.cfm?fuseaction=view_documents&sloc=01-01-08-11-48
    
    A newer toolchain and a linux-nommu port can be fond here:
    http://www.theobroma-systems.com/mico32/
    
    
    
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@890 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ soclib/lib/lm32/src/lm32_debug.cpp
@@ -0,0 +1,227 @@
+/* -*- c++ -*-
+ *
+ * SOCLIB_LGPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU LGPLv2.1.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * SOCLIB_LGPL_HEADER_END
+ *
+ * Copyright (c) TelecomParisTECH
+ *         Tarik Graba <tarik.graba@telecom-paristech.fr>, 2009
+ *
+ * Based on sparcv8 and mips32 code
+ *         Alexis Polti <polti@telecom-paristech.fr>, 2008
+ *         Nicolas Pouillon <nipo@ssji.net>, 2007
+ *         Alain Greiner <alain.greiner@lip6.fr>, 2007
+ *
+ * $Id$
+ *
+ * History:
+ * - 2009-02-15
+ *   Tarik Graba : Forked mips32 and sparcv8 to begin lm32
+ */
+
+#include "lm32.h"
+
+namespace soclib { namespace common {
+
+    void LM32Iss::dump_pc( const std::string &msg) const
+    {
+        std::cout
+            << msg << std::endl
+            << std::hex << std::showbase
+            << "\tPC=" << std::setw(10) << std::setfill('0') << std::internal 
+            << std::hex << std::showbase << r_pc
+            << " / NPC=" << std::setw(10) << std::setfill('0') << std::internal 
+            << std::hex << std::showbase << r_npc
+            << " / next_pc=" << std::setw(10) << std::setfill('0') << std::internal 
+            << std::hex << std::showbase << m_next_pc << std::endl
+            << "\tIns=" << std::setw(10) << std::setfill('0') << std::internal 
+            << std::hex << std::showbase << m_inst.ins
+            << " (" << get_ins_name() << ")" << std::endl ;
+
+    }
+
+    void LM32Iss::dump_regs( const std::string &msg) const
+    {
+    std::cout << msg << std::endl;
+#define DUMP_REG(n, prefix, val)                                    \
+        std::cout <<  prefix << std::setw(2) << std::setfill('0')   \
+        << std::dec << n << ": "                                    \
+        << std::setw(10) << std::setfill('0') << std::internal      \
+        << std::hex << std::showbase << val <<"\t"
+
+#define DUMP_LINE(n)  DUMP_REG(n  ,"r",r_gp[n])   ;\
+                      DUMP_REG(n+1,"r",r_gp[n+1]) ;\
+                      DUMP_REG(n+2,"r",r_gp[n+2]) ;\
+                      std::cout << std::endl
+
+            DUMP_LINE(0);
+            DUMP_LINE(3);
+            DUMP_LINE(6);
+            DUMP_LINE(9);
+            DUMP_LINE(12);
+            DUMP_LINE(15);
+            DUMP_LINE(18);
+            DUMP_LINE(21);
+            DUMP_REG(24,"r",r_gp[24]);
+            DUMP_REG(25,"r",r_gp[25]);
+            std::cout << std::endl;
+
+            DUMP_REG(26,"(gp) r",r_gp[26]);
+            DUMP_REG(27,"(fp) r",r_gp[27]);
+            std::cout << std::endl;
+            DUMP_REG(28,"(sp) r",r_gp[28]);
+            DUMP_REG(29,"(ra) r",r_gp[29]);
+            std::cout << std::endl;
+            DUMP_REG(30,"(bp) r",r_gp[30]);
+            DUMP_REG(31,"(ba) r",r_gp[31]);
+            std::cout << std::endl;
+
+#undef DUMP_LINE
+#undef DUMP_REG
+    }
+
+    std::string LM32Iss::GetExceptioName (int type) const
+    {
+        switch(type){
+#define rstr(x) case x: return #x
+            rstr( X_RESET            );
+            rstr( X_BREAK_POINT      );
+            rstr( X_INST_BUS_ERROR   );
+            rstr( X_WATCH_POINT      );
+            rstr( X_DATA_BUS_ERROR   );
+            rstr( X_DIVISION_BY_ZERO );
+            rstr( X_INTERRUPT        );
+            rstr( X_SYSTEM_CALL      );
+#undef rstr
+            default: return "Unkown exception";
+        }
+    }
+
+    unsigned int LM32Iss::debugGetRegisterCount() const
+    {
+        return 32 + 5;
+    }
+
+    Iss2::debug_register_t LM32Iss::debugGetRegisterValue(unsigned int reg) const
+    {
+        switch (reg)
+        {
+            case 0:
+                return 0;
+            case 1 ... 31:
+                return r_gp[reg];
+            case 32:
+                return r_pc;
+            case 33:
+                return m_exception_cause;
+            case 34:
+                return r_EBA; 
+            case 35:
+                return r_DEBA; 
+            case 36:
+                return r_IE.whole; 
+            default:
+                return 0;
+        }
+    }
+
+    size_t LM32Iss::debugGetRegisterSize(unsigned int reg) const
+    {
+        return 32;
+    }
+
+    void LM32Iss::debugSetRegisterValue(unsigned int reg,  debug_register_t value)
+    {
+        switch (reg)
+        {
+            case 1 ... 31:
+                r_gp[reg] = value;
+                break;
+            case 32:
+                r_pc = value;
+                r_npc = value+4;
+                break;
+            case 33:
+                m_exception_cause = (except_t)value;
+                break;
+            case 34:
+                r_EBA = value;
+                break;
+            case 35:
+                r_DEBA = value;
+                break;
+            case 36:
+                r_IE.whole = value;
+                break;
+            default:
+                break;
+        }
+    }
+
+    Iss2::addr_t LM32Iss::debugGetPC() const
+    {
+        return r_pc;
+    }
+
+    void LM32Iss::debugSetPC(addr_t pc)
+    {
+        r_pc = pc;
+        r_npc = pc+4;
+    }
+
+    int LM32Iss::debugCpuCauseToSignal( uint32_t cause ) const
+    {
+        switch (cause) {
+
+            case  X_RESET             :
+                return 2;       // SIGINT
+            case  X_BREAK_POINT       :
+                return 5;       // GDB SIGTRAP
+            case  X_INST_BUS_ERROR    :
+                return 11;      // SIGSEGV (found nothing better...)
+            case  X_WATCH_POINT       :
+                return 5;       // GDB SIGTRAP
+            case  X_DATA_BUS_ERROR    :
+                return 11;      // SIGSEGV (found nothing better...)
+            case  X_DIVISION_BY_ZERO  :
+                return 8;       // SIGFPE
+            case  X_INTERRUPT         :
+                return 2;       // SIGINT
+            case  X_SYSTEM_CALL       :
+                return 2;       // SIGINT
+            default:  // unkown exception!!
+                {
+                    std::cout << "WARNING: Unkown Exception!!!"<< std::endl;
+                    return 5;
+                }
+
+        }
+    }
+
+}}
+
+// Local Variables:
+// tab-width: 4
+// c-basic-offset: 4
+// c-file-offsets:((innamespace . 0)(inline-open . 0))
+// indent-tabs-mode: nil
+// End:
+
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4
+
---
+++ soclib/lib/lm32/src/lm32_isa.cpp
@@ -0,0 +1,537 @@
+/* -*- c++ -*-
+ *
+ * SOCLIB_LGPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU LGPLv2.1.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * SOCLIB_LGPL_HEADER_END
+ *
+ * Copyright (c) TelecomParisTECH
+ *         Tarik Graba <tarik.graba@telecom-paristech.fr>, 2009
+ *
+ * Based on sparcv8 and mips32 code
+ *         Alexis Polti <polti@telecom-paristech.fr>, 2008
+ *         Nicolas Pouillon <nipo@ssji.net>, 2007
+ *         Alain Greiner <alain.greiner@lip6.fr>, 2007
+ *
+ * $Id$
+ *
+ * History:
+ * - 2009-02-15
+ *   Tarik Graba : Forked mips32 and sparcv8 to begin lm32
+ */
+
+#include "lm32.h"
+
+namespace soclib { namespace common {
+
+    uint32_t LM32Iss::get_absolute_dest_reg(ins_t ins) const
+    {
+        switch(LM32Iss::OpcodesTable [ins.J.op].instformat) {
+            case JI:
+                return 0;
+            case RI:
+                return ins.I.rX;
+            case RR:
+                return ins.R.rX;
+            case CR:
+                return ins.C.rR;
+        }
+        return 0;
+    }
+
+#define OPTABLE(x,y) {&LM32Iss::OP_LM32_##x, #x, y}
+
+    LM32Iss::LM32_op_entry const LM32Iss::OpcodesTable [64]= {
+        OPTABLE(srui   , RI ), OPTABLE(nori  , RI ),
+        OPTABLE(muli   , RI ), OPTABLE(sh    , RR ),
+        OPTABLE(lb     , RR ), OPTABLE(sri   , RI ),  
+        OPTABLE(xori   , RI ), OPTABLE(lh    , RR ),
+        OPTABLE(andi   , RI ), OPTABLE(xnori , RI ),  
+        OPTABLE(lw     , RR ), OPTABLE(lhu   , RR ),
+        OPTABLE(sb     , RR ), OPTABLE(addi  , RI ),  
+        OPTABLE(ori    , RI ), OPTABLE(sli   , RI ),
+        OPTABLE(lbu    , RR ), OPTABLE(be    , RR ),
+        OPTABLE(bg     , RI ), OPTABLE(bge   , RI ),  
+        OPTABLE(bgeu   , RI ), OPTABLE(bgu   , RI ),
+        OPTABLE(sw     , RR ), OPTABLE(bne   , RR ),
+        OPTABLE(andhi  , RI ), OPTABLE(cmpei , RI ),
+        OPTABLE(cmpgi  , RI ), OPTABLE(cmpgei, RI ),
+        OPTABLE(cmpgeui, RI ), OPTABLE(cmpgui, RI ),
+        OPTABLE(orhi   , RI ), OPTABLE(cmpnei, RI ),
+        OPTABLE(sru    , RR ), OPTABLE(nor   , RR ),
+        OPTABLE(mul    , RR ), OPTABLE(divu  , RR ),
+        OPTABLE(rcsr   , CR ), OPTABLE(sr    , RR ),
+        OPTABLE(xor    , RR ), OPTABLE(div   , RR ),
+        OPTABLE(and    , RR ), OPTABLE(xnor  , RR ),
+        OPTABLE(reser  , JI ), OPTABLE(raise , JI ),
+        OPTABLE(sextb  , RR ), OPTABLE(add   , RR ),
+        OPTABLE(or     , RR ), OPTABLE(sl    , RR ),
+        OPTABLE(b      , RR ), OPTABLE(modu  , RR ),
+        OPTABLE(sub    , RR ), OPTABLE(reser , JI ),
+        OPTABLE(wcsr   , CR ), OPTABLE(mod   , RR ),
+        OPTABLE(call   , RR ), OPTABLE(sexth , RR ),
+        OPTABLE(bi     , JI ), OPTABLE(cmpe  , RR ),
+        OPTABLE(cmpg   , RR ), OPTABLE(cmpge , RR ),
+        OPTABLE(cmpgeu , RR ), OPTABLE(cmpgu , RR ),
+        OPTABLE(calli  , JI ), OPTABLE(cmpne , RR ),
+    };
+
+#undef OPTABLE
+
+    std::string LM32Iss::get_ins_name( void ) const
+    {
+        return this->OpcodesTable[m_inst.J.op].name;
+    }
+
+    // Run the instruction 
+    void LM32Iss::run() {
+        // m_inst.J.op contains the opcode
+        // The opcode is the same field for all instruction types
+        void (LM32Iss::*func)() = LM32Iss::OpcodesTable [m_inst.J.op].func;
+        (this->*func)();
+    }
+
+    // The instructions
+#define LM32_function(x) void LM32Iss::OP_LM32_##x()
+
+    LM32_function(raise) {
+        //Soft exception fonction
+        if ((m_inst.ins & 0x7) == 0x7) {
+            m_exception = true; 
+            m_exception_cause = X_SYSTEM_CALL ; // scall instruction
+        }
+        else if((m_inst.ins & 0x3) == 0x2) {
+            m_exception = true; 
+            m_exception_cause = X_BREAK_POINT ; // break instruction
+        }
+        else {
+            std::cout   << name() 
+                << " This raise exception is not implemented !!" 
+                << std::endl;
+            exit (-1);
+        }
+    }
+    LM32_function(reser ) {
+        std::cout << name() << "ERROR: LM32 Iss gets reserved opcode !!" << std::endl;
+        exit (-1);
+    }
+
+    //!Instruction srui behavior method.
+    LM32_function( srui ){// shift rghit unsigned immediate
+        r_gp[m_inst.I.rX] = (unsigned)r_gp[m_inst.I.rY] >> (m_inst.I.imd & 0x1F);
+    }
+
+    //!Instruction nori behavior method.
+    LM32_function( nori ){// not or immediate
+        r_gp[m_inst.I.rX] = ~(r_gp[m_inst.I.rY] | m_inst.I.imd);
+    }
+
+    //!Instruction muli behavior method.
+    LM32_function( muli ){// immediate multiplication
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] * m_inst.I.imd;
+    }
+
+    //!Instruction sri behavior method.
+    LM32_function( sri ){// shift right immediate
+        r_gp[m_inst.I.rX] = (signed)r_gp[m_inst.I.rY] >> (m_inst.I.imd & 0x1F);
+    }
+
+    //!Instruction xori behavior method.
+    LM32_function( xori ){// exclusive or immediate
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] ^ m_inst.I.imd;
+    }
+
+    //!Instruction andi behavior method.
+    LM32_function( andi ){// and immediate
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] & m_inst.I.imd;
+    }
+
+    //!Instruction xnori behavior method.
+    LM32_function( xnori ){// not exclusive or immediate
+        r_gp[m_inst.I.rX] = ~(r_gp[m_inst.I.rY] ^ m_inst.I.imd);
+    }
+
+    //!Instruction addi behavior method.
+    LM32_function( addi ){// add immediate
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] + sign_ext16(m_inst.I.imd);
+    }
+
+    //!Instruction ori behavior method.
+    LM32_function( ori ){// or immediate
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] | m_inst.I.imd;
+    }
+
+    //!Instruction sli behavior method.
+    LM32_function( sli ){// shift left immediate
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] << (m_inst.I.imd & 0x1F);
+    }
+
+    //!Instruction be behavior method.
+    LM32_function( be ){// branch if equal
+        if (r_gp[m_inst.I.rY] == r_gp[m_inst.I.rX])
+        {
+            m_cancel_next_ins = true; // To override r_npc
+            m_next_pc = r_pc+ (sign_ext16(m_inst.I.imd)<<2);
+        }
+    }
+
+    //!Instruction bg behavior method.
+    LM32_function( bg ){// branch if greater
+        if ((signed)r_gp[m_inst.I.rY] > (signed)r_gp[m_inst.I.rX])
+        {
+            m_cancel_next_ins = true; // To override r_npc
+            m_next_pc = r_pc+ (sign_ext16(m_inst.I.imd)<<2);
+        }
+    }
+
+    //!Instruction bge behavior method.
+    LM32_function( bge ){// branch if greater or equal
+        if ((signed)r_gp[m_inst.I.rY] >= (signed)r_gp[m_inst.I.rX])
+        {
+            m_cancel_next_ins = true; // To override r_npc
+            m_next_pc = r_pc+ (sign_ext16(m_inst.I.imd)<<2);
+        }
+    }
+
+    //!Instruction bgeu behavior method.
+    LM32_function( bgeu ){// branch if greater or equal unsigned
+        if ((unsigned)r_gp[m_inst.I.rY] >= (unsigned)r_gp[m_inst.I.rX])
+        {
+            m_cancel_next_ins = true; // To override r_npc
+            m_next_pc = r_pc+ (sign_ext16(m_inst.I.imd)<<2);
+        }
+    }
+
+    //!Instruction bgu behavior method.
+    LM32_function( bgu ){// branch if greater unsigned
+        if ((unsigned)r_gp[m_inst.I.rY] > (unsigned)r_gp[m_inst.I.rX])
+        {
+            m_cancel_next_ins = true; // To override r_npc
+            m_next_pc = r_pc+ (sign_ext16(m_inst.I.imd)<<2);
+        }
+    }
+
+    //!Instruction bne behavior method.
+    LM32_function( bne ){// branch if not equal
+        if (r_gp[m_inst.I.rY] != r_gp[m_inst.I.rX])
+        {
+            m_cancel_next_ins = true; // To override r_npc
+            m_next_pc = r_pc+ (sign_ext16(m_inst.I.imd)<<2);
+        }
+    }
+
+    //!Instruction andhi behavior method.
+    LM32_function( andhi ){// and immediate with high 16bits
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] & (m_inst.I.imd << 16);
+    }
+
+    //!Instruction cmpei behavior method.
+    LM32_function( cmpei ){// compare if equal immediate
+        r_gp[m_inst.I.rX] = ((unsigned) r_gp[m_inst.I.rY] == (unsigned) sign_ext16(m_inst.I.imd));
+    }
+
+    //!Instruction cmpgi behavior method.
+    LM32_function( cmpgi ){// compare if greater  immediate
+        r_gp[m_inst.I.rX] = (signed)r_gp[m_inst.I.rY] >  (signed)sign_ext16(m_inst.I.imd);
+    }
+
+    //!Instruction cmpgei behavior method.
+    LM32_function( cmpgei ){// compare if greater or equal immediate
+        r_gp[m_inst.I.rX] = (signed)r_gp[m_inst.I.rY] >= (signed)sign_ext16(m_inst.I.imd);
+    }
+
+    //!Instruction cmpgeui behavior method.
+    LM32_function( cmpgeui ){// compare if greater or equal immediate unsigned
+        r_gp[m_inst.I.rX] = (unsigned)r_gp[m_inst.I.rY] >= (unsigned)m_inst.I.imd;
+    }
+
+    //!Instruction cmpgui behavior method.
+    LM32_function( cmpgui ){// compare if greater immediate unsigned
+        r_gp[m_inst.I.rX] = (unsigned)r_gp[m_inst.I.rY] > (unsigned)m_inst.I.imd;
+    }
+
+    //!Instruction orhi behavior method.
+    LM32_function( orhi ){// or immediate high 16bits
+        r_gp[m_inst.I.rX] = r_gp[m_inst.I.rY] | (m_inst.I.imd << 16);
+    }
+
+    //!Instruction cmpnei behavior method.
+    LM32_function( cmpnei ){// compare if not equal immediate
+        r_gp[m_inst.I.rX] = ((unsigned)r_gp[m_inst.I.rY] != (unsigned)sign_ext16(m_inst.I.imd));
+    }
+
+    //!Instruction sru behavior method.
+    LM32_function( sru ){// shift right unsigned
+        r_gp[m_inst.R.rX] = (unsigned)r_gp[m_inst.R.rY] >> (r_gp[m_inst.R.rZ] & 0x1F);
+    }
+
+    //!Instruction nor behavior method.
+    LM32_function( nor ){// not or
+        r_gp[m_inst.R.rX] = ~(r_gp[m_inst.R.rY] | r_gp[m_inst.R.rZ]);
+    }
+
+    //!Instruction mul behavior method.
+    LM32_function( mul ){// multiplication
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] * r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction divu behavior method.
+    LM32_function( divu ){// unsigned integer division
+        if (r_gp[m_inst.R.rZ] == 0){
+            m_exception = true;
+            m_exception_cause = X_DIVISION_BY_ZERO; // division by 0
+        }
+        else
+            r_gp[m_inst.R.rX] = (unsigned)r_gp[m_inst.R.rY] / (unsigned)r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction sr behavior method.
+    LM32_function( sr ){// shift right signed
+        r_gp[m_inst.R.rX] = (signed)r_gp[m_inst.R.rY] >> (r_gp[m_inst.R.rZ] & 0x1F);
+    }
+
+    //!Instruction xor behavior method.
+    LM32_function( xor ){// exclusive or
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] ^ r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction div behavior method.
+    LM32_function( div ){// integer division
+        if (r_gp[m_inst.R.rZ] == 0){
+            m_exception = true;
+            m_exception_cause = X_DIVISION_BY_ZERO; // division by 0
+        }
+        else
+            r_gp[m_inst.R.rX] = (signed)r_gp[m_inst.R.rY] / (signed)r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction and behavior method.
+    LM32_function( and ){
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] & r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction xnor behavior method.
+    LM32_function( xnor ){// not exclusive or
+        r_gp[m_inst.R.rX] = ~(r_gp[m_inst.R.rY] ^ r_gp[m_inst.R.rZ]);
+    }
+
+    //!Instruction sextb behavior method.
+    LM32_function( sextb ){// sign extension of a byte
+        r_gp[m_inst.R.rX] = ((signed)(r_gp[m_inst.R.rY]<<24))>>24;
+    }
+
+    //!Instruction add behavior method.
+    LM32_function( add ){// addition
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] + r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction or behavior method.
+    LM32_function( or ){// or
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] | r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction sl behavior method.
+    LM32_function( sl ){// shift left
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] << (r_gp[m_inst.R.rZ] & 0x1F);
+    }
+
+    //!Instruction b behavior method.
+    LM32_function( b ){ // branch
+        m_cancel_next_ins = true; // To override r_npc
+        m_next_pc = r_gp[m_inst.R.rY];
+        if (m_inst.R.rY == 30)      // eret // return from exception
+            r_IE.IE = r_IE.EIE;
+        else if (m_inst.R.rY == 31) // bret // return from breakpoint
+            r_IE.IE = r_IE.BIE;
+    }
+
+    //!Instruction modu behavior method.
+    LM32_function( modu ){// unsigned modulo
+        if (r_gp[m_inst.R.rZ] == 0){
+            m_exception = true;
+            m_exception_cause = X_DIVISION_BY_ZERO; // division by 0
+        }
+        else
+            r_gp[m_inst.R.rX] = (unsigned)r_gp[m_inst.R.rY] % (unsigned)r_gp[m_inst.R.rZ]; 
+    }
+
+    //!Instruction sub behavior method.
+    LM32_function( sub ){// soustraction
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] - r_gp[m_inst.R.rZ]; 
+    }
+
+    //!Instruction mod behavior method.
+    LM32_function( mod ){// signed modulo
+        if (r_gp[m_inst.R.rZ] == 0){
+            m_exception = true;
+            m_exception_cause = X_DIVISION_BY_ZERO; // division by 0
+        }
+        else
+            r_gp[m_inst.R.rX] = (signed)r_gp[m_inst.R.rY] % (signed)r_gp[m_inst.R.rZ]; 
+    }
+
+    //!Instruction call behavior method.
+    LM32_function( call ){// jump to sub routine
+        r_gp[ra] = r_npc ;// is pc + 4!!// return address
+        m_cancel_next_ins = true; // To override r_npc
+        m_next_pc = r_pc+ r_gp[m_inst.R.rY];
+    }
+
+    //!Instruction calli behavior method.
+    LM32_function( calli ){//jump to sub routine immediate
+        r_gp[ra] = r_npc ; // is pc + 4!!// return address
+        m_cancel_next_ins = true; // To override r_npc
+        m_next_pc = r_pc + (sign_ext26(m_inst.J.imd)<<2);
+    }
+
+    //!Instruction bi behavior method.
+    LM32_function( bi ){// branch immediate
+        m_cancel_next_ins = true; // To override r_npc
+        m_next_pc = r_pc + (sign_ext26(m_inst.J.imd)<<2);
+    }
+
+    //!Instruction sexth behavior method.
+    LM32_function( sexth ){// sign extension of a half
+        r_gp[m_inst.R.rX] = ((signed)(r_gp[m_inst.R.rY]<<16))>>16;
+    }
+
+    //!Instruction cmpe behavior method.
+    LM32_function( cmpe ){// compare if equal
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] == r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction cmpg behavior method.
+    LM32_function( cmpg ){// compare if greater signed
+        r_gp[m_inst.R.rX] = (signed)r_gp[m_inst.R.rY] > (signed)r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction cmpge behavior method.
+    LM32_function( cmpge ){// compare if greater or equal signed
+        r_gp[m_inst.R.rX] = (signed)r_gp[m_inst.R.rY] >= (signed)r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction cmpgeu behavior method.
+    LM32_function( cmpgeu ){// compare if greater or equal unsigned
+        r_gp[m_inst.R.rX] = (unsigned) r_gp[m_inst.R.rY] >= (unsigned) r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction cmpgu behavior method.
+    LM32_function( cmpgu ){// compare if greater unsigned
+        r_gp[m_inst.R.rX] = (unsigned) r_gp[m_inst.R.rY] > (unsigned) r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction cmpne behavior method.
+    LM32_function( cmpne ){// compare if not equal
+        r_gp[m_inst.R.rX] = r_gp[m_inst.R.rY] != r_gp[m_inst.R.rZ];
+    }
+
+    //!Instruction rcsr behavior method.
+    LM32_function( rcsr ){// read control & status register
+        switch (m_inst.C.csr){
+            case 0x0:  // interrupt enable
+                r_gp[m_inst.C.rR] = r_IE.whole ;
+                break;
+            case 0x1:  // interrupt mask
+                r_gp[m_inst.C.rR] = r_IM ;
+                break;
+            case 0x2:  // interrupt pending
+                r_gp[m_inst.C.rR] = r_IP ; 
+                break;
+            case 0x3:  // inst cache control
+                r_gp[m_inst.C.rR] = 0x0; // write only!! no indication about the read value
+                break;
+            case 0x4:  // data cache control
+                r_gp[m_inst.C.rR] = 0x0; // write only!! no indication about the read value
+                break;
+            case 0x5:  // cycle counter
+                r_gp[m_inst.C.rR] = r_CC ;
+                break;
+            case 0x6:  // conf register 
+                r_gp[m_inst.C.rR] = r_CFG.whole ;
+                break;
+            case 0x7:  // Exception base address
+                r_gp[m_inst.C.rR] = r_EBA ;
+                break;
+            default:
+                std::cout   << name()
+                    << "Error: Read to Unkown CSR !!"<<std::endl;
+                exit (-1);
+                break;
+        }
+    }
+
+    // XTN_CACHE must be XTN_DCACHE_INVAL or XTN_ICACHE_INVAL
+#define FLUSH(XTN_CACHE)                                       \
+    do {                                                       \
+        struct DataRequest null_dreq = ISS_DREQ_INITIALIZER;   \
+        m_dreq.req = null_dreq;                                \
+        m_dreq.sign_extend = false;                            \
+        m_dreq.dest_reg = 0;                                   \
+        m_dreq.addr = 0;                                       \
+        m_dreq.req.valid = true;                                      \
+        m_dreq.req.addr = XTN_CACHE*4;                                  \
+        m_dreq.req.wdata = 0;                                         \
+        m_dreq.req.be = 0;                                            \
+        m_dreq.req.type = XTN_WRITE;                                 \
+        m_dreq.req.mode = MODE_USER;                                  \
+    } while(0)
+
+    //!Instruction wcsr behavior method.
+    LM32_function( wcsr ){// write control & status register
+        uint32_t wData = r_gp[m_inst.C.rW];
+        switch (m_inst.C.csr){
+            case 0x0: // interrupt enable
+                r_IE.IE = 0x1 & wData;
+                break;
+            case 0x1: // interrupt mask
+                r_IM = wData;
+                break;
+            case 0x2: // interrupt pending
+                for (int i=0; i<32; i++){
+                    // Bits are cleared by writing '1'!!!
+                    if (wData>>i)
+                        r_IP = r_IP & ~(1<<i);
+                }
+                break;
+            case 0x3:  // inst cache ctrl
+                FLUSH(XTN_ICACHE_INVAL);
+                r_ICC  = 0x1; //a write invalidate the Icache
+                break;
+            case 0x4:  // data cache ctrl
+                FLUSH(XTN_DCACHE_INVAL);
+                r_DCC  = 0x1; //a read invalidate de Dcache
+                break;
+            case 0x5: // cycle counter 
+                //r_CC  = wData; //read only
+                break;
+            case 0x6: // cfg register
+                //r_CFG = wData; //read only
+                break;
+            case 0x7: // Exception base address
+                r_EBA  = wData & 0xFFFFFF00;
+                break;
+            default:
+                std::cout   << name()
+                    << "Error: Write to Unkown CSR !!"<<std::endl;
+                exit (-1);
+                break;
+        }
+    }
+#undef FLUSH
+#undef LM32_function
+}}
