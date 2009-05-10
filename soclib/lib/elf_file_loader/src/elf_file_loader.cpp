b8cf9d2ce1f1fec1f9038db83e7b6fdde091d3a7
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-05-10 13:01:56 +0000

    Also load symbol sections
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1076 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/lib/elf_file_loader/src/elf_file_loader.cpp
+++ soclib/lib/elf_file_loader/src/elf_file_loader.cpp
@@ -83,25 +83,42 @@ bool elf_load( const std::string &filename, Loader &loader )
                 flags,
                 actual_size,
                 blob ));
+
+        FOREACH( symp, sect->get_symbol_table() )
+        {
+            elfpp::symbol &sym = *(symp->second);
+            size_t symsize = sym.get_size();
+            uint8_t info = sym.get_info();
+
+            if ( ( ELF_ST_BIND(info) != elfpp::STB_LOCAL &&
+                   ELF_ST_BIND(info) != elfpp::STB_GLOBAL ) ||
+                 ELF_ST_TYPE(info) >= elfpp::STT_NUM )
+                continue;
+
+            uintptr_t addr =
+                (sym.get_section() ? sym.get_section()->get_load_address() : 0)
+                + sym.get_value();
+            loader.addSymbol(BinaryFileSymbol( sym.get_name(), addr, symsize ));
+        }
     }
-    
+
     FOREACH( symp, binary->get_symbol_table() )
     {
         elfpp::symbol &sym = *(symp->second);
         size_t symsize = sym.get_size();
-
         uint8_t info = sym.get_info();
-        if ( ELF_ST_BIND(info) != elfpp::STB_LOCAL &&
-             ELF_ST_BIND(info) != elfpp::STB_GLOBAL )
-            continue;
-        if ( ELF_ST_TYPE(info) >= elfpp::STT_NUM )
+
+        if ( ( ELF_ST_BIND(info) != elfpp::STB_LOCAL &&
+               ELF_ST_BIND(info) != elfpp::STB_GLOBAL ) ||
+             ELF_ST_TYPE(info) >= elfpp::STT_NUM )
             continue;
 
         uintptr_t addr =
             (sym.get_section() ? sym.get_section()->get_load_address() : 0)
             + sym.get_value();
-        loader.addSymbol(
-            BinaryFileSymbol( sym.get_name(), addr, symsize ));
+
+        if ( addr )
+            loader.addSymbol(BinaryFileSymbol( sym.get_name(), addr, symsize ));
     }
 
     delete binary;
