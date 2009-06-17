adccf0bd4a2a0fbbbdaa2161d2f3d18645d5ccdd
alinevieiramello@hotmail.com <alinevieiramello@hotmail.com@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-06-17 14:07:23 +0000

    add tlmdt-vgmn-multi_timer-mips32 platform
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1167 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ soclib/platform/topcells/tlmdt-vgmn-multi_timer-mips32/soft/deps.mk
@@ -0,0 +1,11 @@
+system.o:  \
+ /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/system.c \
+  /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/system.h \
+  /dsk/l1/misc/alinev/soclib/soclib/module/connectivity_component/vci_multi_tty/include/soclib/tty.h \
+  /dsk/l1/misc/alinev/soclib/soclib/lib/include/soclib_io.h \
+  /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/stdint.h \
+  ../segmentation.h \
+  /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/stdint.h \
+  /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/system_mips.h \
+  /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/stdio.h \
+  /dsk/l1/misc/alinev/soclib/soclib/platform/topcells/common/stdlib.h
