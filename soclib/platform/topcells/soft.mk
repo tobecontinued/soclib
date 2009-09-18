f256bf79f883fd9cf0575588b5cc96528d2339bf
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-09-18 10:52:45 +0000

    Make soft flags from platform desc more reliable
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1259 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/platform/topcells/soft.mk
+++ soclib/platform/topcells/soft.mk
@@ -33,7 +33,7 @@ OBJS?=main.o exception.o system.o $(ADD_OBJS)
 COMMON=$(SOCLIB)/soclib/platform/topcells/common
 include $(SOCLIB)/utils/conf/soft_flags.mk
 
-INTERFACE_CFLAGS:=$(shell soclib-cc -I $(PLATFORM_DIR) -p $(PLATFORM_DESC) $(SOCLIB_CC_ADD_ARGS) --embedded-cflags)
+INTERFACE_CFLAGS:=$(shell cd $(PLATFORM_DIR) && soclib-cc -p $(PLATFORM_DESC) $(SOCLIB_CC_ADD_ARGS) --embedded-cflags)
 
 VPATH=. $(COMMON)
 
