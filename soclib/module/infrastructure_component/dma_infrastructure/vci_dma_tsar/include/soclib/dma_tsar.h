f041ea04f11e379a1e66167ec206ff7b6f1adc54
eric.guthmuller@polytechnique.edu <eric.guthmuller@polytechnique.edu@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-09-21 10:01:25 +0000

    Add Tsar DMA, which respects the tsar specifications for memory access
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1264 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ soclib/module/infrastructure_component/dma_infrastructure/vci_dma_tsar/include/soclib/dma_tsar.h
@@ -0,0 +1,51 @@
+/*
+ * SOCLIB_LGPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU LGPLv2.1.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * SOCLIB_LGPL_HEADER_END
+ *
+ * Copyright (c) UPMC, Lip6, Asim
+ *         Nicolas Pouillon <nipo@ssji.net>, 2007
+ *         Eric Guthmuller  <eric.guthmuller@polytechnique.edu>, 2009
+ *
+ * Maintainers: nipo eric.guthmuller@polytechnique.edu
+ */
+#ifndef DMA_REGS_TSAR_H
+#define DMA_REGS_TSAR_H
+
+#include "soclib_io.h"
+
+enum SoclibDmaRegisters {
+    DMA_SRC,
+    DMA_DST,
+    DMA_LEN,
+    DMA_RESET,
+    DMA_IRQ_DISABLED,
+};
+
+#endif /* DMA_REGS_TSAR_H */
+
+// Local Variables:
+// tab-width: 4
+// c-basic-offset: 4
+// c-file-offsets:((innamespace . 0)(inline-open . 0))
+// indent-tabs-mode: nil
+// End:
+
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4
+
