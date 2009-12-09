74fff2164a0e0013a99328d724b571a26ff9c214
abdelmalek.si-merabet@lip6.fr <abdelmalek.si-merabet@lip6.fr@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-12-09 14:22:43 +0000

    adding ShellSort
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1472 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/platform/topcells/caba-ring-ccxcachev1_memcachev1-mipsel/soft/main.c
+++ soclib/platform/topcells/caba-ring-ccxcachev1_memcachev1-mipsel/soft/main.c
@@ -1,44 +1,37 @@
 #include "system.h"
 #include "stdio.h"
 #include "stdlib.h"
-
+#include "matrice.h"
 
 #include "../segmentation.h"
 
 #define NPROCS 4
-#define SIZE 50
-#define SORT_TYPE 1
+#define SIZE 1000 
+#define SORT_TYPE 0 //3
 
-uint32_t lock=0;
-volatile int compteur=NPROCS;
+//extern int _malek; //_gp;
+
 volatile int nprocs=NPROCS;
 
-// 100 elements
-const unsigned int gQSortNum0[100] = {
-  251,  24, 113, 170, 215,  60,  83,  30, 115,  48, 169, 210,  49,  92, 101,  58,  21, 184, 225,   6,
-  199, 244, 227, 146,  99,  96,  25, 222,  65, 140, 213,  22, 219, 136, 175, 182,  73,  36, 141, 190, 
-  83, 112, 137, 114, 175, 188, 187, 102,  53, 168, 193, 154, 167, 172,   3, 242,  67,  64, 144, 137,
-  142, 175, 188,  69, 102,  53, 168, 193, 102,  89, 172, 253, 242,  67, 192,   7,  62, 159,  20, 181,
-  182, 187, 216, 207,  22, 105, 132, 109, 162, 205,  16, 151,  18, 113, 228,  37,   6,  85,   8, 161
-};
+unsigned int SortArr0[NPROCS*(SIZE+200)];
+//unsigned int SortArr0[4*4*SIZE];
 
-unsigned int SortArr0[NPROCS*(SIZE)];
-
-void SORT(int *base, int n, int size, int (*compar) (),int type);
-void QSORT(int *base, int n, int size, int (*compar) ());
-void simple_sort(int *base, int n, int size, int (*compar) ());
-void insertion_sort(int *base, int n, int size, int (*compar) ());
-void bubble_sort(int *base, int n, int size, int (*compar) ());
-int compare(int *n1, int *n2)
-{
-  return (*((unsigned int *) n1) - *((unsigned int *) n2));
-}
+void SORT(unsigned int *base, unsigned int n, int type);
+void insertion_sort(unsigned int *base, unsigned int n); // type 2
+void selection_sort(unsigned int *base, unsigned int n); // type 1
+void bubble_sort(unsigned int *base, unsigned int n);    // type 3
+void shellSortPhase(unsigned int a[],unsigned int length, int gap);
+void shellSort(unsigned int *base, unsigned int n);     // type 0
 
 int main()
 {
-
-
   register int p; 
+
+  int beg_cycle, end_cycle;
+
+  beg_cycle = cpu_cycles();
+
+//  printf("_malek = %p\n", &_malek);   
 
   p=procnum();
 
@@ -46,329 +39,216 @@ int main()
   putchar(p+'0');
   putchar('\n');
 
-
   int i;
   int j;
-  int temp;
-
   unsigned int* SortArray;
-  if(p+1 <= nprocs){
-    for(i=1 ; i <= 40 ; i++ ){
-      puts("Memory copy \n");
-      unsigned int increment = 4*(p+i);
-      increment = increment % 16;
-      SortArray = SortArr0 + p*SIZE;
-      memcpy(SortArray, (gQSortNum0 + increment) ,SIZE*4);
-      puts("Sort... \n");
-      SORT((int *) (SortArray), (int) SIZE, sizeof(unsigned int), compare,SORT_TYPE);
-      for (j = 1; j < SIZE; j++)
-      {
-        if (SortArray[j] < SortArray[j - 1])
-        {
-          puts("ucbqsort: failed\n");
-          while(1);
-        }
-      }
-      puts("ucbqsort: success\n");
-    }
-  }
 
-
-  if(p+1 <= nprocs){
-    lock_lock(&lock);
-    compteur--;
-    putchar('\n');
-    puti(compteur);
-    putchar('\n');
-    lock_unlock(&lock);
-  }
-
-  puts("Display the sorted array : \n");
-  for(j = 0; j < SIZE; j++)
+  if(p+1 <= nprocs)
   {
-    puti(SortArray[j]);
-    putchar('\n');
+        i=1;
+  	puts("Memory copy \n");
+        SortArray = SortArr0 + p*(SIZE+200);
+	memcpy(SortArray, gQSortNum0 + p*SIZE,SIZE*4);
+	//memcpy(SortArray, gQSortNum0,SIZE*4);
+
+ 	puts("Sort... \n");
+        SORT((unsigned int *) (SortArray), (unsigned int) SIZE, SORT_TYPE);
+
+  	for (j = 1; j < SIZE; j++)
+    	{
+	  if (SortArray[j] < SortArray[j-1])
+		{
+	  		puts("ucbqsort: failed\n");
+			while(1);
+		}
+
+    	}
+
+  	puts("ucbqsort: success\n");
+        end_cycle = cpu_cycles();
+        printf( "nombre cycles cpu : %i\n", end_cycle-beg_cycle);
   }
+
+  
+// puts("Display the sorted array : \n");
+// for(j = 0; j < SIZE; j++)
+// {
+//   puti(SortArray[j]);
+//   putchar('\n');
+// }
+
+//  printf( "------------------------------ \n");
+//  printf( "nombre cycles cpu : %i\n", end_cycle-beg_cycle);
+//  printf( "------------------------------ \n");
 
 
   while(1);
 }
 
-static int (*qcmp) ();
-static int qsz;
-static int thresh;
-static int mthresh;
-static void qst(int *, int *);
-void QSORT(base, n, size, compar)
-  int *base;
-  int n;
-  int size;
-  int (*compar) ();
+
+//---- insertion sort : non adapté pour tableaux de grande taille (> 100) --
+void insertion_sort(unsigned int *base, unsigned int n) 
 {
-  register int c, *i, *j, *lo, *hi;
-  int *min, *max;
-  if (n <= 1)
-    return;
-  qsz = size;
-  qcmp = compar;
-  thresh = 4;
-  mthresh = 6;
-  max = base + n;
-  if (n >= 4)
-  {
-    qst(base, max);
-    hi = base + thresh;
-  } 
-  else
-  {
-    hi = max;
-  }
+    /* Spécifications externes : Tri du tableau base par insertion séquentielle */
+    int i,p,j;
+    int x;
 
-  puts("Quick sort done... \n");
+    puts("Insertion Sort\n");
 
-  for (j = lo = base; (lo ++) < hi;)
-    if ((*qcmp) (j, lo) > 0)
-      j = lo;
-  if (j != base)
-  {
-    for (i = base, hi = base + 1; i < hi;)
+    for (i = 1; i < n; i++) 
     {
-      c = *j;
-      *j++ = *i;
-      *i++ = c;
+
+        putchar('-'); // added for debug
+
+        /* stockage de la valeur en i */
+        x = base[i]; 
+ 
+        /* recherche du plus petit indice p inférieur à i tel que base[p] >= base[i] */
+        for(p = 0; base[p] < x; p++);
+        /* p pointe une valeur de base supérieure à celle en i */ 
+ 
+        /* décalage avant des valeurs de base entre p et i */         
+        for (j = i-1; j >= p; j--) {
+            base[j+1] = base[j]; 
+        }   
+ 
+        base[p] = x; /* insertion de la valeur stockée à la place vacante */
+
+        putchar('+'); // added for debug
+
     }
-
-  }
-  for (min = base; (hi = min ++) < max;)
-  {
-    while ((*qcmp) (hi --, min) > 0)
-      ;
-    if ((hi ++) != min)
-    {
-      for (lo = min + 1; --lo >= min;)
-      {
-        c = *lo;
-        for (i = j = lo; (j --) >= hi; i = j)
-          *i = *j;
-        *i = c;
-      }
-
-    }
-  }
-
 }
 
-
-void simple_sort(base, n, size, compar)
-  int *base;
-  int n;
-  int size;
-  int (*compar) ();
+//------ simple_sort -------------------------------
+void selection_sort(unsigned int *base, unsigned int n)
 {
-  register int c, *i, *j, *lo, p, *k;
-  int *max;
-  if (n <= 1)
-    return;
-  qsz = size;
-  qcmp = compar;
-  max = base + n;
-  puts("Selection Sort\n");
-  p=procnum();
-  //  k=4;
-  for(j = base; j < max ; j++){
-    lo=j;
-    c=(*lo);
-    for(i = j; i < max ; i++){
-      asm("	nop		\n"); //1
-      if( c-(*i) > 0){
-        lo=i;
-        c=(*lo);
-      }
-      asm("	nop		\n"); // 1
-    }
-    *lo = *j;
-    *j = c;
-  }
+     int i, min, j , x;
+     puts("Selection Sort\n");
+
+     for(i = 0 ; i < n - 1 ; i++)
+     {
+
+         putchar('-'); // added for debug
+
+         min = i;
+
+         
+         for(j = i+1 ; j < n ; j++)
+         {
+        
+            if(base[j] < base[min])
+                  min = j;
+            
+         }
+
+         if(min != i)
+         {
+             x = base[i];
+             base[i] = base[min];
+             base[min] = x;
+         }
+
+         putchar('+'); // added for debug
+
+     }
 }
-
-void insertion_sort(base, n, size, compar)
-  int *base;
-  int n;
-  int size;
-  int (*compar) ();
+//-------------------------------
+void bubble_sort(unsigned int *base, unsigned int n)
 {
-  register int c, *i, *j,p;
-  int *max;
-  if (n <= 1)
-    return;
-  qsz = size;
-  qcmp = compar;
-  max = base + n;
-  puts("Insertion Sort\n");
-  p=procnum();
-  for(j = base; j < max-1 ; j++){
-    for(i = j; i < max-1 ; i++){
-      if( (*i)-(*(i+1)) > 0){
-        c = *i;
-        *i = *(i+1);
-        *(i+1)=c;
-      }
-    }
-  }
+        int i   = 0; /* Indice de répétition du tri */
+ 	int j   = 0; /* Variable de boucle */
+ 	int tmp = 0; /* Variable de stockage temporaire */
+        int en_desordre = 1; /* Booléen marquant l'arrêt du tri si le tableau est ordonné */
+
+        puts("Bubble Sort\n");
+
+	/* Boucle de répétition du tri et le test qui arrête le tri dès que le tableau est ordonné */
+	for(i = 0 ; (i < n) && en_desordre; i++)
+	{
+                putchar('-'); // added for debug
+
+		/* Supposons le tableau ordonné */
+		en_desordre = 0;
+		/* Vérification des éléments des places j et j-1 */
+		for(j = 1 ; j < n - i ; j++)
+		{
+			/* Si les 2 éléments sont mal triés */
+			if(base[j] < base[j-1])
+			{
+				/* Inversion des 2 éléments */
+ 				tmp = base[j-1];
+ 				base[j-1] = base[j];
+ 				base[j] = tmp;
+ 
+ 				/* Le tableau n'est toujours pas trié */
+				en_desordre = 1;
+ 			}
+		}
+
+                putchar('+'); // added for debug
+	}
+
 }
-
-
-void bubble_sort(base, n, size, compar)
-  int *base;
-  int n;
-  int size;
-  int (*compar) ();
-{
-  register int c, *i;
-  register int b_swap=1;
-  int *max;
-  if (n <= 1)
-    return;
-  qsz = size;
-  qcmp = compar;
-  max = base + n;
-  puts("Bubble Sort\n");
-  while(b_swap){
-    b_swap=0;
-    for(i = base; i < max-1 ; i++){
-      if( (*i)-(*(i+1)) > 0){
-        b_swap=1;
-        c = *i;
-        *i = *(i+1);
-        *(i+1)=c;
-      }
-    }
-  }
-}
-
-  static
-void qst(base, max)
-  int *base, *max;
-{
-  register int c, *i, *j, *jj;
-  register int ii;
-  int *mid, *tmp;
-  int lo, hi;
-  lo = max - base;
-  do
-  {
-    mid = i = base + 1 * ((lo / qsz) >> 1);
-    if (lo >= mthresh)
-    {
-      j = ((*qcmp) ((jj = base), i) > 0 ? jj : i);
-      if ((*qcmp) (j, (tmp = max - 1)) > 0)
-      {
-        j = (j == jj ? i : jj);
-        if ((*qcmp) (j, tmp) < 0)
-          j = tmp;
-      }
-
-      if (j != i)
-      {
-        ii = 1;
-        do
-        {
-          c = *i;
-          *i++ = *j;
-          *j++ = c;
+//------------------------------------------------------
+/*
+ * Exécute un tri par insertion avec la séparation donnée
+ * If gap == 1, on fait un tri ordinaire.
+ * If gap >= length, on ne fait rien.
+ */
+void shellSortPhase(unsigned int a[],unsigned int length, int gap) {
+    int i;
+ 
+    puti(gap);
+    for (i = gap; i < length; ++i) {
+        unsigned int value = a[i];
+        int j;
+        for (j = i - gap; j >= 0 && a[j] > value; j -= gap) {
+	    putchar('+');
+            a[j + gap] = a[j];
+	    putchar('-');
         }
-
-        while (--ii);
-      }
-
+        a[j + gap] = value;
     }
-    for (i = base, j = max - 1;;)
-    {
-      while (i < mid && (*qcmp) (i, mid) <= 0)
-        i ++;
-      while (j > mid)
-      {
-        if ((*qcmp) (mid, j) <= 0)
-        {
-          j --;
-          continue;
-        }
-
-        tmp = i + 1;
-        if (i == mid)
-        {
-          mid = jj = j;
-        } 
-        else
-        {
-          jj = j;
-          j --;
-        }
-
-        goto swap;
-      }
-
-      if (i == mid)
-      {
-        break;
-      } 
-      else
-      {
-        jj = mid;
-        tmp = mid = i;
-        j --;
-      }
-
-swap:
-      ii = 1;
-      do
-      {
-        c = *i;
-        *i++ = *jj;
-        *jj++ = c;
-      }
-
-      while (--ii);
-      i = tmp;
-    }
-
-    i = (j = mid) + 1;
-    if ((lo = j - base) <= (hi = max - i))
-    {
-      if (lo >= thresh)
-        qst(base, j);
-      base = i;
-      lo = hi;
-    } 
-    else
-    {
-      if (hi >= thresh)
-        qst(i, max);
-      max = j;
-    }
-
-  }
-  while (lo >= thresh);
+}
+ 
+void shellSort(unsigned int *base, unsigned int n) {
+    /*
+     * gaps[] doit approximer une Série géométrique.
+     * La sequence suivante est la meilleure connue en terme
+     * de nombre moyen de comparaisons. voir:
+     * http://www.research.att.com/~njas/sequences/A102549
+     */
+    static const int gaps[] = {
+        1, 4, 10, 23, 57, 132, 301, 701
+    };
+    int sizeIndex;
+ 
+    puts("Shell Sort\n");
+    for (sizeIndex = sizeof(gaps)/sizeof(gaps[0]) - 1;
+               sizeIndex >= 0;
+               --sizeIndex)
+        shellSortPhase(base, n, gaps[sizeIndex]);
 }
 
-void SORT(int *base, int n, int size, int (*compar) (),int type){
-  switch(type){
-    case 0:
-      QSORT(base, n, size, compar);
-      break;
-    case 1:
-      simple_sort(base, n, size, compar);
-      break;
-    case 2:
-      insertion_sort(base, n, size, compar);
-      break;
-    case 3:
-      bubble_sort(base, n, size, compar);
-      break;
-    default:
-      break;
+//-------------------------------------*/
+void SORT(unsigned int *base, unsigned int n, int type)
+{
+  switch(type)
+  {
+  case 0:
+    shellSort(base, n);
+    break;
+  case 1:
+    selection_sort(base, n);
+    break;
+  case 2:
+    insertion_sort(base, n);
+    break;
+  case 3:
+    bubble_sort(base, n);
+    break;
+  default:
+    break;
   }
-
 }
-
 
