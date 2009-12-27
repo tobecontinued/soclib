b22389217f0dbc79dd294309058a652ef12d081a
polti <polti@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2009-12-27 17:49:54 +0000

    Corrected Sparc IRQ handling
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1495 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/iss/sparcv8/src/sparcv8.cpp
+++ soclib/iss/sparcv8/src/sparcv8.cpp
@@ -1,23 +1,23 @@
 /* -*- c++ -*-
  *
  * SOCLIB_LGPL_HEADER_BEGIN
- * 
+ *
  * This file is part of SoCLib, GNU LGPLv2.1.
- * 
+ *
  * SoCLib is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation; version 2.1 of the License.
- * 
+ *
  * SoCLib is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with SoCLib; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA
- * 
+ *
  * SOCLIB_LGPL_HEADER_END
  *
  * Copyright (c) Telecom ParisTech
@@ -47,16 +47,15 @@ tmpl(/**/)::Sparcv8Iss(const std::string &name, uint32_t ident)
         std::cout << "The windows number must be larger than 2, and no larger than 32." << std::endl;
         exit(1);
     }
-    
+
     // WIM : unimplemented windows bit are read-only and must read as 0
-    r_wim      = 0xffffffff & ~(0xffffffff << NWINDOWS);    
-    
+    r_wim      = 0xffffffff & ~(0xffffffff << NWINDOWS);
 }
 
 tmpl(void)::reset()
 {
     struct DataRequest null_dreq = ISS_DREQ_INITIALIZER;
-    
+
     m_ibe                       = false;
     m_dbe                       = false;
     m_dreq.req                  = null_dreq;
@@ -66,7 +65,7 @@ tmpl(void)::reset()
     m_hazard                    = false;
     m_exception                 = false;
     m_exception_cause           = TP_RESET;
-    
+
     r_psr.impl  = 0xf;   // Soclib sparcv implementation
     r_psr.ver   = 0x1;   // Implementation version
     r_psr.et    = 1;     // Enable reset trap.
@@ -78,7 +77,7 @@ tmpl(void)::reset()
     r_psr.ef    = 0;     // No floating-point unit
 #endif
     r_psr.pil   = 0xf;   // No ext IRQ enabled
-    r_psr.cwp   = 0;     // Current window pointer. 
+    r_psr.cwp   = 0;     // Current window pointer.
 
     // Simulate a reset exception
     r_tbr.whole = 0;
@@ -94,7 +93,7 @@ tmpl(void)::reset()
     r_error_mode = false;
   }
 
-// The processor always asks for an instruction. 
+// The processor always asks for an instruction.
 // TODO : this is suboptimal, could be optimised out (only ask for an intruction
 //        if it is necessary)
 tmpl(void)::getRequests( struct InstructionRequest &ireq,
@@ -130,7 +129,7 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
         if (irsp.valid) {
             // Swap instruction bytes (sparc is big endian), and store it for later use
             m_ins.ins = soclib::endian::uint32_swap(irsp.instruction);
-    
+
             // Instruction bus erros are signaled synchronously
             m_ibe = irsp.error;
 
@@ -139,10 +138,10 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
             // the pipeline.
             // To detect pipeline stall, we use the absolute number of the registers (ie. we take in
             // consideration the CWP), as the CWP may change between the LOAD and the faulty instruction.
-            // To be honnest, with the current SocLib implementation, this is not strictly necessary, 
-            //  but things may change, and it dosen't cost much. So let's do it the most versatile way.
+            // To be honnest, with the current SocLib implementation, this is not strictly necessary,
+            //  but things may change, and it doesn't cost much. So let's do it the most versatile way.
             uint32_t dest_reg = get_absolute_dest_reg(m_ins);
-            m_hazard = ((dest_reg == m_dreq.dest_reg) && m_dreq.req.valid) 
+            m_hazard = ((dest_reg == m_dreq.dest_reg) && m_dreq.req.valid)
                 || ((dest_reg == m_dreq.ext_dest_reg) && m_dreq.ext_req.valid);
         }
     }
@@ -152,12 +151,12 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
 
     // At each cycle we execute an instruction and the data transfert for the preceding instruction.
     // If either following condition is true :
-    //    - no instruction response (m_ireq_pending), 
+    //    - no instruction response (m_ireq_pending),
     //    - a data request pending and still no data reponse (m_dreq_pending == true)
-    //    - we are in the middle of a multicycle instruction, 
-    // then do nothing and advance time as far as possible : 
+    //    - we are in the middle of a multicycle instruction,
+    // then do nothing and advance time as far as possible :
     //    - if we are not in the middle of a long instruction : ncycle
-    //    - if we are in the middle of a long instruction : to the end of current instruction 
+    //    - if we are in the middle of a long instruction : to the end of current instruction
     if (m_ireq_pending || m_dreq_pending || m_ins_delay) {
       uint32_t t = ncycle;
       if (m_ins_delay) {
@@ -167,15 +166,15 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
       }
 
 #ifdef SOCLIB_MODULE_DEBUG
-      std::cout << name() << " frozen :" 
+      std::cout << name() << " frozen :"
                 << " m_ireq_pending=" << m_ireq_pending
                 << " mdreq_pending=" << m_dreq_pending
-                << " m_ins_delay=" << m_ins_delay 
+                << " m_ins_delay=" << m_ins_delay
                 << std::endl;
 #endif
       return t;
     }
-    
+
     // Pipeline is 2 deep. So hazard only last 2 cycles.
     // If there is a hazard and we were asked to execute two cycles or more, then
     // we pretend that the first cycle was executed, and that we now do the second with
@@ -186,11 +185,11 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
     if ( m_hazard && ncycle > 1 ) {
       ncycle = 2;
       m_hazard = false;
-    } 
+    }
     else {
       ncycle = 1;
     }
-    
+
     // Initialize future value of npc.
     // In case of jump, the instruction will modify it.
     m_next_pc = r_npc+4;
@@ -204,7 +203,7 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
     }
 
     // Instruction bus exceptions are set / reset synchronously at each cycle by wrapper.
-    // Data bus exceptions are only SET by wrapper (and maybe asynchronously). 
+    // Data bus exceptions are only SET by wrapper (and maybe asynchronously).
     // So we need to record that an exception has occured, then RESET the flip flop ourselves.
     if ( m_dbe ) {
       m_exception = true;
@@ -215,7 +214,7 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
 
 #ifdef SOCLIB_MODULE_DEBUG
     std::cout << m_name << std::endl;
-    dump_pc("Before instruction execution :"); 
+    dump_pc("Before instruction execution :");
     dump_regs("");
 #endif
 
@@ -226,12 +225,9 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
       std::cout << name() << " hazard, seeing next cycle" << std::endl;
 #endif
       goto house_keeping;
-    } 
-    else {
-      run();
     }
 
-    // Current instruction has been executed, let's now handle external interrupts.
+    // Let's now handle external interrupts if there is any
     if (r_psr.et && ((irq_bit_field == 0xf) || (irq_bit_field > r_psr.pil))) {
       m_exception = true;
       m_exception_cause = TP_INTERRUPT_LEVEL(irq_bit_field);
@@ -239,11 +235,15 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
 #ifdef SOCLIB_MODULE_DEBUG
       std::cout << name() << " Taking irqs " << irq_bit_field << std::endl;
     } else {
-      if ( irq_bit_field )
+      if (irq_bit_field)
         std::cout << name() << " Ignoring irqs " << irq_bit_field << std::endl;
 #endif
     }
 
+    // Execute currrent instruction
+      run();
+
+    // Let's handle the case where the current instruction caused an exception
     {
         ExceptionClass ex_class = EXCL_FAULT;
         ExceptionCause ex_cause = EXCA_OTHER;
@@ -298,8 +298,9 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
             ;
         }
         // Let's now handle traps... or let GDB handle them for us :)
-        if (m_exception && debugExceptionBypassed( ex_class, ex_cause ))
-            m_exception = false;
+        if (m_exception && debugExceptionBypassed( ex_class, ex_cause )) {
+                m_exception = false;
+        }
     }
 
     if (!m_exception)
@@ -309,36 +310,36 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
 
     {
       m_exception = false;
-      
+
       // Double exception ?
       if (r_psr.et == 0) {
-        std::cout 
+        std::cout
           << m_name
           << " A precise trap (" << GetTrapName(m_exception_cause) << ")"
           << " occured at pc = " << std::setw(10) << std::setfill('0') << std::internal << std::hex << std::showbase << r_pc
           << " and the Enable Trap flag is not set." << std::endl
-          << " Entering Error Mode and rebooting." 
+          << " Entering Error Mode and rebooting."
           << std::endl;
 
           r_error_mode = true;
-        
+
         // Reset processor
         m_exception_cause = TP_RESET;
       }
       else {
         // Disable traps
         r_psr.et = 0;
-        
+
         // Save processor state
         r_psr.ps = r_psr.s;
-        
+
         // Enter supervisor mode
         r_psr.s = 1;
-        
+
         // Allocate a new window
         r_psr.cwp = (r_psr.cwp - 1) % NWINDOWS;
-        
-        // Save PC and NPC. 
+
+        // Save PC and NPC.
         // A synchronous DBE is related to the *previous* instruction. So in this case, we save the
         // previous PC, hopping that the trap handler will have a mean to redo the faulty instruction.
         // If the DBE was asynchronous, we're lost. This is the best we can do...
@@ -351,20 +352,20 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
           GPR(18) = r_npc;
         }
       }
-      
+
       // Save Trap type
       r_tbr.tt = m_exception_cause;
-      
+
       // Branch to trap handler location
-      if (m_exception_cause == TP_RESET) 
+      if (m_exception_cause == TP_RESET)
         m_next_pc = 0;
-      else 
+      else
         m_next_pc = r_tbr.whole;
 
 #ifdef SOCLIB_MODULE_DEBUG
       std::cout
-        << m_name 
-        << " exception: " 
+        << m_name
+        << " exception: "
         << std::setw(10) << std::setfill('0') << std::internal << std::hex << std::showbase << ((unsigned long)m_exception_cause)
         << " (" << GetTrapName(m_exception_cause) << ")"
         << std::endl
@@ -385,7 +386,6 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
 #endif
         << std::endl;
 #endif
-
       m_cancel_next_ins = true;
     }
 
@@ -395,7 +395,7 @@ tmpl(uint32_t)::executeNCycles( uint32_t ncycle,
       r_pc = m_next_pc;
       r_npc = m_next_pc+4;
       m_cancel_next_ins = false;
-    } 
+    }
     else {
       r_pc = r_npc;
       r_npc = m_next_pc;
@@ -427,4 +427,3 @@ tmpl()::~Sparcv8Iss()
 // End:
 
 // vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4
-
