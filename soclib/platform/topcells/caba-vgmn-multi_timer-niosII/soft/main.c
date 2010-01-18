e17e76a88d89ed4321a33a7fe14d9f993db1b36d
irisa <irisa@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2010-01-18 09:53:27 +0000

    Updating of the software running on the caba-vgmn-multi_timer-niosII platform
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1520 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/platform/topcells/caba-vgmn-multi_timer-niosII/soft/main.c
+++ soclib/platform/topcells/caba-vgmn-multi_timer-niosII/soft/main.c
@@ -1,32 +1,32 @@
 /* -*- c++ -*-
  *
  * SOCLIB_LGPL_HEADER_BEGIN
- * 
+ *
  * This file is part of SoCLib, GNU LGPLv2.1.
- * 
+ *
  * SoCLib is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation; version 2.1 of the License.
- * 
+ *
  * SoCLib is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with SoCLib; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA
- * 
+ *
  * SOCLIB_LGPL_HEADER_END
- * 
+ *
  * Copyright (c) UPMC, Lip6, SoC
  *         Nicolas Pouillon <nipo@ssji.net>, 2006-2007
  *
  *
  */
 
-#include "soclib/timer.h"
+#include "timer.h"
 #include "system.h"
 #include "stdio.h"
 
@@ -41,7 +41,7 @@ void irq_handler(int irq)
   uint32_t ti;
 
   int left = atomic_add(&max_interrupts, -1);
-	
+
   ti = soclib_io_get(
 		     base(TIMER),
 		     procnum()*TIMER_SPAN+TIMER_VALUE);
@@ -61,7 +61,7 @@ int main(void)
   const int cpu = procnum();
 
   printf("hello From Nios II processor %d\n", procnum());
-	
+
   set_irq_handler(irq_handler);
   enable_hw_irq(0);
   irq_enable();
