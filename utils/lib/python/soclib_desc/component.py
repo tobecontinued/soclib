0843989c5c02223f59db3e561a31bde6e7533280
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2010-04-14 10:26:11 +0000

    Avoid serializing modules
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1700 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- utils/lib/python/soclib_desc/component.py
+++ utils/lib/python/soclib_desc/component.py
@@ -34,128 +34,137 @@ __id__ = "$Id$"
 __version__ = "$Revision$"
 
 __all__ = ['Module', 'PortDecl',
-		   'MetaSignal', 'MetaPortDecl', 'SubSignal',
-		   'parameter', 'types',
-		   'Signal','Port']
+           'MetaSignal', 'MetaPortDecl', 'SubSignal',
+           'parameter', 'types',
+           'Signal','Port']
 
 class SubConn:
-	def __init__(self, type, name, count = None, auto = None, **args):
-		self.__type = type
-		self.__name = name
-		self.__count = count
-		self.__auto = auto
-		self.__args = args
-		self.where = traceback.extract_stack()[-2][0:2]
-	def Use(self):
-		return Uses(self.__type, **self.__args)
-	def getSpec(self, **args):
-		from specialization import Specialization
-		a = {}
-		a.update(args)
-		a.update(self.__args)
-		return Specialization(self.__type, **a)
-	def getMetaInfo(self):
-		return self.__type, self.__name, self.__count
-	def getInfo(self, **args):
-		ptype = self.getSpec(**args)
-		return self.__name, ptype, self.__count, self.__auto
-	def __str__(self):
-		from specialization import Specialization
-		ptype = Specialization(self.__type, **self.__args)
-		return '<%s: %s %s>'%(self.__class__.__name__,
-							  self.__name,
-							  str(ptype['header_files']))
-	def __repr__(self):
-		args = ', '.join(['%s = %r'%(k, self.__args[k]) for k in sorted(filter(lambda x:':' not in x, self.__args.keys()))])
-		return 'Port("%s", "%s", %s)'%(self.__type,
-									   self.__name,
-									   args)
+    def __init__(self, type, name, count = None, auto = None, **args):
+        self.__type = type
+        self.__name = name
+        self.__count = count
+        self.__auto = auto
+        self.__args = args
+        self.where = traceback.extract_stack()[-2][0:2]
+    def Use(self):
+        return Uses(self.__type, **self.__args)
+    def getSpec(self, **args):
+        from specialization import Specialization
+        a = {}
+        a.update(args)
+        a.update(self.__args)
+        return Specialization(self.__type, **a)
+    def getMetaInfo(self):
+        return self.__type, self.__name, self.__count
+    def getInfo(self, **args):
+        ptype = self.getSpec(**args)
+        return self.__name, ptype, self.__count, self.__auto
+    def __str__(self):
+        from specialization import Specialization
+        ptype = Specialization(self.__type, **self.__args)
+        return '<%s: %s %s>'%(self.__class__.__name__,
+                              self.__name,
+                              str(ptype['header_files']))
+    def __repr__(self):
+        args = ', '.join(['%s = %r'%(k, self.__args[k])
+                          for k in sorted(filter(lambda x: ':' not in x and
+                                                 isinstance(self.__args[x],
+                                                            (str, unicode, int)),
+                                                 self.__args.keys()))])
+        return 'Port("%s", "%s", %s)'%(self.__type,
+                                       self.__name,
+                                       args)
 
 class Port(SubConn):
-	pass
+    pass
 
 class SubSignal(SubConn):
-	pass
+    pass
 
 class Signal(Module):
-	tb_delta = Module.tb_delta-1
-	def __init__(self, name, **kwargs):
-		self.__accepts = kwargs['accepts']
-		del kwargs['accepts']
-		Module.__init__(self, name, **kwargs)
+    tb_delta = Module.tb_delta-1
+    def __init__(self, name, **kwargs):
+        self.__accepts = kwargs['accepts']
+        del kwargs['accepts']
+        Module.__init__(self, name, **kwargs)
 
-	def __getitem__(self, key):
-		from soclib_desc import description_files
-		if key == 'accepts':
-			naccepts = {}
-			for type, count in self.__accepts.iteritems():
-				rtype = description_files.get_module(type)
-				naccepts[rtype] = count
-			return naccepts
-		else:
-			return Module.__getitem__(self, key)
+    def __getitem__(self, key):
+        from soclib_desc import description_files
+        if key == 'accepts':
+            naccepts = {}
+            for type, count in self.__accepts.iteritems():
+                rtype = description_files.get_module(type)
+                naccepts[rtype] = count
+            return naccepts
+        else:
+            return Module.__getitem__(self, key)
 
 class PortDecl(Module):
-	def __getitem__(self, key):
-		from soclib_desc import description_files
-		if key == 'signal':
-			sig = Module.__getitem__(self, 'signal')
-			if sig:
-				return description_files.get_module(sig)
-		return Module.__getitem__(self, key)
+    def __getitem__(self, key):
+        from soclib_desc import description_files
+        if key == 'signal':
+            sig = Module.__getitem__(self, 'signal')
+            if sig:
+                return description_files.get_module(sig)
+        return Module.__getitem__(self, key)
 
 class MetaPortDecl(PortDecl):
-	pass
+    pass
 
 class MetaSignal(Signal):
-	pass
+    pass
 
 class Uses:
-	"""
-	A statement declaring the platform uses a specific component (in a
-	global meaning, ie hardware component or utility).
+    """
+    A statement declaring the platform uses a specific component (in a
+    global meaning, ie hardware component or utility).
 
-	name is the name of the component, it the filename as in
-	desc/soclib/[component].sd
+    name is the name of the component, it the filename as in
+    desc/soclib/[component].sd
 
-	args is the list of arguments useful for compile-time definition
-	(ie template parameters)
-	"""
-	def __init__(self, name, **args):
-		self.name = name
-		self.args = args
-		for k in filter(lambda x:':' in x, self.args.keys()):
-			del self.args[k]
+    args is the list of arguments useful for compile-time definition
+    (ie template parameters)
+    """
+    def __init__(self, name, **args):
+        self.name = name
+        self.args = args
 
-		self.__hash = (
-			hash(self.name)^
-			hash(str([(k, self.args[k]) for k in sorted(self.args)])))
+        for k in filter(lambda x:':' in x, self.args.keys()):
+            del self.args[k]
 
-		# This is for error feedback purposes
-		self.where = traceback.extract_stack()[-2][0:2]
+        self.__hash = (
+            hash(self.name)^
+            hash(str([(k, self.args[k]) for k in sorted(self.args)])))
 
-	def specialization(self, **args):	
-		from specialization import Specialization
-		a = {}
-		a.update(args)
-		a.update(self.args)
-#		print 'use', `self`
-#		from pprint import pprint
-#		pprint(a)
-		return Specialization(self.name, __use = self.where, **a)
-		
-	def __str__(self):
-		return '<Use %s from %s>'%(self.name, self.where)
+        # This is for error feedback purposes
+        self.where = traceback.extract_stack()[-2][0:2]
 
-	def __repr__(self):
-		args = ', '.join(['%s = %r'%(k, self.args[k]) for k in sorted(filter(lambda x:':' not in x, self.args.keys()))])
-		return 'Uses(%r, %s)'%(self.name, args)
+    def specialization(self, **args):   
+        from specialization import Specialization
+        a = {}
+        a.update(args)
+        a.update(self.args)
+#       print 'use', `self`
+#       from pprint import pprint
+#       pprint(a)
+        return Specialization(self.name, __use = self.where, **a)
+        
+    def __str__(self):
+        return '<Use %s from %s>'%(self.name, self.where)
 
-	def __cmp__(self, other):
-		return (
-			cmp(self.name, other.name) or
-			cmp(self.args, other.args)
-			)
+    def __repr__(self):
+        args = ', '.join(['%s = %r'%(k, self.args[k])
+                          for k in sorted(filter(lambda x: ':' not in x and
+                                                 isinstance(self.args[x],
+                                                            (str, unicode, int)),
+                                                 self.args.keys()))])
+        return 'Uses(%r, %s)'%(self.name, args)
 
-	def __hash__(self):
-		return self.__hash
+    def __cmp__(self, other):
+        return (
+            cmp(self.name, other.name) or
+            cmp(self.args, other.args)
+            )
+
+    def __hash__(self):
+        return self.__hash
