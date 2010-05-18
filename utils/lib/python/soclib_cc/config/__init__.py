f402a760e0fe026f2ea1cbb7ad84a57eb4e977c5
nipo <nipo@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2010-05-18 11:28:18 +0000

    Upgrade configuration system
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1736 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- utils/lib/python/soclib_cc/config/__init__.py
+++ utils/lib/python/soclib_cc/config/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 # SOCLIB_GPL_HEADER_BEGIN
 # 
@@ -19,103 +18,29 @@
 # 02110-1301, USA.
 # 
 # SOCLIB_GPL_HEADER_END
-# 
-# Copyright (c) UPMC, Lip6, SoC
-#         Nicolas Pouillon <nipo@ssji.net>, 2007
-# 
-# Maintainers: group:toolmakers
 
-import os, os.path
-from pprint import pprint
-from utils import *
-import types
-
+__author__ = 'Nicolas Pouillon, <nipo@ssji.net>'
+__copyright__ = 'UPMC, Lip6, SoC, 2007-2010'
+__license__ = 'GPL-v2'
 __id__ = "$Id$"
 __version__ = "$Revision$"
 
-__all__ = ['config', 'Joined']
+__doc__ = """
+Soclib-cc configuration engine. In this file, we only create the
+configuration spool and set the root "config" variable.
+"""
 
-import re
+import os
+import os.path
+import warnings
 
-# Yes, its ugly and lisp-ish, but who cares ?
-class ConfigSpool:
-    _filter = re.compile("[a-z][a-z_]+")
-    _fmt = '%s:\n%s'
+__all__ = ['config']
 
-    _desc_paths = ["soclib/communication", 'soclib/lib', 'soclib/module', 'soclib/iss']
-
-    def addDescPath(self, np):
-        if not np in self._desc_paths:
-            self._desc_paths.append(np)
-    addDescPath = classmethod(addDescPath)
-
-    def _pprint(self, obj, pf='  '):
-        def _repr(o):
-            try:
-                if issubclass(o, Configurator):
-                    return '\n'+self._pprint(o, pf+'  ')
-            except TypeError:
-                pass
-            return repr(o)
-        return '\n'.join(map(lambda k:pf+'%s: %s'%(k, _repr(getattr(obj, k))),
-                             filter(self._filter.match, dir(obj))))
-
-    def __str__(self):
-        return '\n'.join(map(lambda k:self._fmt%(k, self._pprint(getattr(self, k), '  ')),
-                             filter(self._filter.match, dir(self))))
-
-    def __iter__(self):
-        for i in filter(self._filter.match, dir(self)):
-            obj = getattr(self, i)
-            if not isinstance(obj, (types.FunctionType, types.MethodType)):
-                yield obj
-
-    def __getitem__(self, name):
-        return getattr(self, name)
+from soclib_cc.config import spool
 
 _cur_soclib = os.path.abspath(
-    os.path.join(
-    os.path.dirname(__file__),
+    os.path.join(os.path.dirname(__file__),
     '../../../../..'))
 assert(_cur_soclib)
 
-def include(filename, glbl):
-    name = os.path.join(_cur_soclib, filename)
-    if os.path.isfile(name):
-        exec open(name) in glbl, {}
-
-def parseall():
-    config = ConfigSpool()
-    glbl = {'Config':Config,
-            'config':config,
-            'include':include}
-    for name in (os.path.join(os.path.dirname(__file__), 'templates.py'),
-                 os.path.join(_cur_soclib, 'utils', 'conf', 'soclib.conf'),
-                 os.path.expanduser("~/.soclib/global.conf"),
-                 "soclib.conf"):
-        if os.path.isfile(name):
-            exec open(name) in glbl, {}
-    for c in config:
-        c._do_expands()
-    return config
-
-_configs = parseall()
-config = _configs.default
-config.doConfigure(_cur_soclib, _configs._desc_paths)
-config.type = 'default'
-
-def change_config(name):
-    newconf = getattr(_configs, name)
-    from soclib_cc import config
-    newconf.doConfigure(_cur_soclib, _configs._desc_paths)
-    config.config.__dict__ = newconf.__dict__
-    for n in dir(newconf):
-        setattr(config.config, n, getattr(newconf, n))
-    config.type = name
-
-def Joined(i):
-    return ' '.join(map(lambda x:'"%s"'%x, i))
-
-if __name__ == "__main__":
-    print config
-
+config = spool.ConfigSpool(_cur_soclib)
---
+++ utils/lib/python/soclib_cc/config/objects.py
@@ -0,0 +1,267 @@
+
+# SOCLIB_GPL_HEADER_BEGIN
+# 
+# This file is part of SoCLib, GNU GPLv2.
+# 
+# SoCLib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+# 
+# SoCLib is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with SoCLib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+# 
+# SOCLIB_GPL_HEADER_END
+
+__author__ = 'Nicolas Pouillon, <nipo@ssji.net>'
+__copyright__ = 'UPMC, Lip6, SoC, 2007-2010'
+__license__ = 'GPL-v2'
+__id__ = "$Id$"
+__version__ = "$Revision$"
+
+import os
+import os.path
+
+__all__ = ['BuildEnv', 'Library', 'Toolchain', 'Config', '_pformat']
+
+def _pformat(data, indent, dict_delim = ['dict(', ')']):
+    '''
+    Pretty printer capable of handling:
+     * dicts
+     * lists and tuples (printed as lists)
+     * Config (uses pprint())
+     * other datatypes (using repr)
+    '''
+    r = ''
+    if isinstance(data, dict):
+        r += dict_delim[0]
+        for k in sorted(data.keys()):
+            v = data[k]
+            r += '\n'
+            r += indent + '  ' + k + ' = '
+            r += _pformat(v, indent+'    ')
+            r += ','
+        r += '\n'
+        r += indent + dict_delim[1]
+    elif isinstance(data, (list, tuple)):
+        r += '['
+        for i in data:
+            r += '\n'
+            r += indent + '  '
+            r += _pformat(i, indent+'  ')
+            r += ','
+        r += '\n'
+        r += indent + ']'
+    elif isinstance(data, Config):
+        r += data.pformat(indent)
+    else:
+        r += repr(data)
+    return r
+
+class Config:
+    '''
+    Base configuration object, handles all the attribute
+    dereferencing.
+
+    Also handles the %()s expansion when attributes are referenced
+    '''
+    def __init__(self, parent = None, **kwargs):
+        '''
+        parent is an optional parent Config.
+        kwargs are set values
+        '''
+        self.__parent = parent
+        self.__args = kwargs
+
+    def __getitem__(self, key):
+        """
+        This gets a value from the __args, if not found, recurses down
+        to parents.
+
+        Using __getitem__ permits writing 'str % self'
+        """
+        try:
+            return self.__args[key]
+        except:
+            pass
+        if self.__parent:
+            return self.__parent[key]
+        raise KeyError(key)
+
+    def __remap(self, value):
+        '''
+        Does the %()s expansion on the value given. It silently
+        ignores anything other than a string.
+        '''
+        if not isinstance(value, basestring):
+            return value
+        old = ''
+        while old != value:
+            old, value = value, os.path.expandvars(value % self)
+        return value
+
+    def __getattr__(self, key):
+        '''
+        Does the magical attribute getting hackery.
+         * Supports getting all variables present in __args
+         * Passes strings, dicts, lists and tuples through the
+           remapping feature
+        '''
+        if key.startswith('__'):
+            raise AttributeError(key)
+        value = self[key]
+        if isinstance(value, (list, tuple)):
+            return map(self.__remap, value)
+        if isinstance(value, dict):
+            return dict([(k, self.__remap(v)) for
+                         (k, v) in value.items()])
+        if isinstance(value, str):
+            value = self.__remap(value)
+        return value
+
+#    def set(self, key, val):
+#        self.__args[key] = val
+
+    @property
+    def parent(self):
+        '''
+        Parent of current Config, None if no parent.
+        '''
+        return self.__parent
+
+    def pformat(self, indent = ''):
+        '''
+        Pretty printer. Basically a str() with an optional indent
+        argument.
+        '''
+        data = {}
+        p = self
+        while p:
+            for k, v in p.__args.items():
+                data.setdefault(k, v)
+            p = p.parent
+        return _pformat(data, indent,
+                        dict_delim = [self.__class__.__name__+'(', ')'])
+
+    def get_flags(self, *flags):
+        '''
+        A getter helper for flags, where we need a concatenation of
+        <something>_cflags and cflags. Concatenes all lists found
+        named after *flags. Silently ignored if not found.
+        '''
+        flag_list = []
+        for f in flags:
+            try:
+                flag_list += getattr(self, f)
+            except:
+                pass
+        return flag_list
+
+class Toolchain(Config):
+    '''
+    Implements toolchain specificities
+    '''
+
+    def get_tool(self, name, mode):
+        '''
+        Retrieves a tool in the tool_map field. Recurses down to
+        parent until found.
+        '''
+        tn = mode+'_'+name
+        _name = name
+        if mode and tn in self.tool_map:
+            _name = tn
+        if _name in self.tool_map:
+            tool = self.tool_map[_name]
+            if isinstance(tool, str):
+                return tool.split(' ')
+            else:
+                return tool
+        if self.parent:
+            return self.parent.get_tool(name, mode)
+        raise KeyError(name)
+
+class Library(Config):
+    pass
+
+class BuildEnv(Config):
+
+    # Private API
+
+    def __get_flags(self, *flags):
+        flag_list = []
+        for l in self.libraries:
+            flag_list += l.get_flags(*flags)
+        flag_list += self.toolchain.get_flags(*flags)
+        return flag_list
+
+    def __get_tool(self, name, mode):
+        """
+        Retrieves a tool in the toolchain's tool_map field. Recurses
+        down to parent until found. Beware of double parent recursion:
+         * <me>
+           * <me.toolchain>
+             * <me.toolchain.parent>
+               * ...
+         * <me.parent.toolchain>
+           * <me.parent.toolchain.parent>
+             * ...
+        """
+        try:
+            return self.toolchain.get_tool(name, mode)
+        except KeyError:
+            pass
+        finally:
+            pass
+        if self.parent:
+            return self.parent.__get_tool(name, mode)
+        return [self.toolchain.prefix + name]
+
+    # User API
+
+    def getTool(self, name, mode = ''):
+        '''
+        Use API helper, see __get_tool
+        '''
+        return self.__get_tool(name, mode)
+
+    def getCflags(self, mode=None):
+        '''
+        Use API helper, see __get_flags and Config.get_flags
+        '''
+        if mode is None:
+            from soclib_cc.config import config
+            mode = config.mode
+        return self.__get_flags(mode+'_cflags', 'cflags')
+
+    def getLibs(self, mode=None):
+        '''
+        Use API helper, see __get_flags and Config.get_flags
+        '''
+        if mode is None:
+            from soclib_cc.config import config
+            mode = config.mode
+        return self.__get_flags(mode+'_libs', 'libs')
+
+    def reposFile(self, name, mode=None):
+        '''
+        Retrieves an unique filename based on the name and the
+        "repos" location.
+        '''
+        if mode is None:
+            from soclib_cc.config import config
+            mode = config.mode
+        if config.max_name_length:
+            if len(name) > config.max_name_length:
+                name, ext = os.path.splitext(name)
+                name = 'ln_'+hex(hash(name))+ext
+        r = os.path.join(self.repos, mode, name)
+        if config.max_name_length:
+            r = r.replace(':','_')
+        return r
