ea969abd99f0f0387254973403699a4fe88085fd
alinev <alinev@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2010-10-01 12:14:11 +0000

    Using  RUN_FOR(1) in mips32
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@1934 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/iss/mips32/src/mips32.cpp
+++ soclib/iss/mips32/src/mips32.cpp
@@ -214,7 +214,7 @@ uint32_t Mips32Iss::executeNCycles(
     if ( ncycle == 0 )
         goto early_end;
 
-    RUN_FOR(ncycle);
+    RUN_FOR(1);
 
     if ( dreq_ok && ireq_ok ) {
 #ifdef SOCLIB_MODULE_DEBUG
