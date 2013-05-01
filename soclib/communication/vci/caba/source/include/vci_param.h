c357d05c040baa656ec2c7af4af17dc64b9b56b1
alain <alain@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2013-05-01 15:04:48 +0000

    Defining two new classes: VciParamsBis and VciParamsTer, that have the same member variables than VciParams, with the same semantic, but with different names: cell_size is replaced by cell_size_bis or cell_size_ter. This is actually a temporary (?) patch to solve a soclib-cc limitation.
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@2323 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/communication/vci/caba/source/include/vci_param.h
+++ soclib/communication/vci/caba/source/include/vci_param.h
@@ -50,9 +50,9 @@ static std::string VciParamsString(
     return o.str();
 }
 
-/**
+/********************************************
  * VCI parameters grouped in a single class
- */
+ ********************************************/
 template<
     int cell_size,
     int plen_size,
@@ -161,20 +161,245 @@ public:
     {
         fast_data_t ret = (be * 0x0100040010004000ULL | be * 0x0002000800200080ULL) & 0x8080808080808080ULL;
         ret |= ret >> 1; ret |= ret >> 2; ret |= ret >> 4;
-#if 0
-        fast_data_t ret = 0;
-        const fast_data_t be_up = 1 << (B - 1);
-
-        for (size_t i = 0; i < (size_t)B; ++i) {
-            ret <<= 8;
-            if ( be_up & be )
-                ret |= 0xff;
-            be <<= 1;
-        }
-#endif
         return ret;
     }
-};
+};  // end class VciParams
+
+/****************************************************
+ * This class VciParamsBis can be used when a module
+ * has two VCI ports with different parameters.
+ * This is actually a temporary (?) patch to solve
+ * a soclib-cc limitation.
+ ****************************************************/
+template<
+    int cell_size_bis,
+    int plen_size_bis,
+    int addr_size_bis,
+    int rerror_size_bis,
+    int clen_size_bis,
+    int rflag_size_bis,
+    int srcid_size_bis,
+    int pktid_size_bis,
+    int trdid_size_bis,
+	int wrplen_size_bis
+    >
+class VciParamsBis
+{
+	/* Obey standart */
+
+	// This is a check for a pow of 2
+    soclib_static_assert(!((cell_size_bis)&(cell_size_bis-1)));
+	soclib_static_assert(plen_size_bis <= 9);
+    // We need more than 32 bits for addr, so we dont check
+    //soclib_static_assert(addr_size_bis <= 32);
+	soclib_static_assert(rerror_size_bis <= 3);
+	soclib_static_assert(clen_size_bis <= 8);
+    // We need more than 5 bits for srcid, so we dont check
+    //soclib_static_assert(srcid_size_bis <= 5);
+	soclib_static_assert(pktid_size_bis <= 8);
+	soclib_static_assert(wrplen_size_bis <= 5);
+
+public:
+    /* Standart's constants, may be used by some modules */
+    static const int B = cell_size_bis;
+    static const int K = plen_size_bis;
+    static const int N = addr_size_bis;
+    static const int E = rerror_size_bis;
+    static const int Q = clen_size_bis;
+    static const int F = rflag_size_bis;
+    static const int S = srcid_size_bis;
+    static const int P = pktid_size_bis;
+    static const int T = trdid_size_bis;
+    static const int W = wrplen_size_bis;
+
+    /* The basic signal types */
+	/* Handshake */
+	typedef bool ack_t;
+	typedef bool val_t;
+	/* Request content */
+	typedef sc_dt::sc_uint<N> addr_t;
+	typedef sc_dt::sc_uint<B> be_t;
+	typedef bool cfixed_t;
+	typedef sc_dt::sc_uint<Q> clen_t;
+	typedef sc_dt::sc_uint<2> cmd_t;
+	typedef bool contig_t;
+	typedef sc_dt::sc_uint<B*8> data_t;
+	typedef bool eop_t;
+	typedef bool const_t;
+	typedef sc_dt::sc_uint<K> plen_t;
+	typedef bool wrap_t;
+	/* Response content */
+	typedef sc_dt::sc_uint<E> rerror_t;
+
+	/* The advanced signal types */
+	/* Request content */
+	typedef bool defd_t;
+	typedef sc_dt::sc_uint<W> wrplen_t;
+	/* Response content */
+	typedef sc_dt::sc_uint<F> rflag_t;
+	/* Threading */
+	typedef sc_dt::sc_uint<S> srcid_t;
+	typedef sc_dt::sc_uint<T> trdid_t;
+	typedef sc_dt::sc_uint<P> pktid_t;
+
+    typedef typename ::soclib::common::fast_int_t<addr_size_bis>::int_t fast_addr_t;
+    typedef typename ::soclib::common::fast_int_t<cell_size_bis*8>::int_t fast_data_t;
+    typedef typename ::soclib::common::fast_int_t<trdid_size_bis>::int_t fast_trdid_t;
+
+    enum {
+        CMD_NOP,
+        CMD_READ,
+        CMD_WRITE,
+        CMD_LOCKED_READ,
+        CMD_STORE_COND = CMD_NOP,
+    };
+
+    static const unsigned int _err_mask = (1<<rerror_size_bis)-1;
+    typedef enum {
+        ERR_NORMAL = 0 & _err_mask,
+        ERR_GENERAL_DATA_ERROR = 1 & _err_mask,
+        ERR_BAD_DATA = 5 & _err_mask,
+        ERR_ABORT_DISCONNECT = 7 & _err_mask,
+    } vci_error_e;
+
+    enum {
+        STORE_COND_ATOMIC = 0,
+        STORE_COND_NOT_ATOMIC = 1,
+    };
+
+    static std::string string( const std::string &name = "" )
+    {
+        std::string vp = VciParamsString(B,K,N,E,Q,F,S,P,T,W);
+        if ( name == "" )
+            return vp;
+        return name+'<'+vp+'>';
+    }
+
+    static inline fast_data_t be2mask( fast_data_t be )
+    {
+        fast_data_t ret = (be * 0x0100040010004000ULL | be * 0x0002000800200080ULL) & 0x8080808080808080ULL;
+        ret |= ret >> 1; ret |= ret >> 2; ret |= ret >> 4;
+        return ret;
+    }
+};  // end class VciParamsBis
+
+/****************************************************
+ * This class VciParamsTer can be used when a module
+ * has three VCI ports with different parameters.
+ * This is actually a temporary (?) patch to solve
+ * a soclib-cc limitation.
+ ****************************************************/
+template<
+    int cell_size_ter,
+    int plen_size_ter,
+    int addr_size_ter,
+    int rerror_size_ter,
+    int clen_size_ter,
+    int rflag_size_ter,
+    int srcid_size_ter,
+    int pktid_size_ter,
+    int trdid_size_ter,
+	int wrplen_size_ter
+    >
+class VciParamsTer
+{
+	/* Obey standart */
+
+	// This is a check for a pow of 2
+    soclib_static_assert(!((cell_size_ter)&(cell_size_ter-1)));
+	soclib_static_assert(plen_size_ter <= 9);
+    // We need more than 32 bits for addr, so we dont check
+    //soclib_static_assert(addr_size_ter <= 32);
+	soclib_static_assert(rerror_size_ter <= 3);
+	soclib_static_assert(clen_size_ter <= 8);
+    // We need more than 5 bits for srcid, so we dont check
+    //soclib_static_assert(srcid_size_ter <= 5);
+	soclib_static_assert(pktid_size_ter <= 8);
+	soclib_static_assert(wrplen_size_ter <= 5);
+
+public:
+    /* Standart's constants, may be used by some modules */
+    static const int B = cell_size_ter;
+    static const int K = plen_size_ter;
+    static const int N = addr_size_ter;
+    static const int E = rerror_size_ter;
+    static const int Q = clen_size_ter;
+    static const int F = rflag_size_ter;
+    static const int S = srcid_size_ter;
+    static const int P = pktid_size_ter;
+    static const int T = trdid_size_ter;
+    static const int W = wrplen_size_ter;
+
+    /* The basic signal types */
+	/* Handshake */
+	typedef bool ack_t;
+	typedef bool val_t;
+	/* Request content */
+	typedef sc_dt::sc_uint<N> addr_t;
+	typedef sc_dt::sc_uint<B> be_t;
+	typedef bool cfixed_t;
+	typedef sc_dt::sc_uint<Q> clen_t;
+	typedef sc_dt::sc_uint<2> cmd_t;
+	typedef bool contig_t;
+	typedef sc_dt::sc_uint<B*8> data_t;
+	typedef bool eop_t;
+	typedef bool const_t;
+	typedef sc_dt::sc_uint<K> plen_t;
+	typedef bool wrap_t;
+	/* Response content */
+	typedef sc_dt::sc_uint<E> rerror_t;
+
+	/* The advanced signal types */
+	/* Request content */
+	typedef bool defd_t;
+	typedef sc_dt::sc_uint<W> wrplen_t;
+	/* Response content */
+	typedef sc_dt::sc_uint<F> rflag_t;
+	/* Threading */
+	typedef sc_dt::sc_uint<S> srcid_t;
+	typedef sc_dt::sc_uint<T> trdid_t;
+	typedef sc_dt::sc_uint<P> pktid_t;
+
+    typedef typename ::soclib::common::fast_int_t<addr_size_ter>::int_t fast_addr_t;
+    typedef typename ::soclib::common::fast_int_t<cell_size_ter*8>::int_t fast_data_t;
+    typedef typename ::soclib::common::fast_int_t<trdid_size_ter>::int_t fast_trdid_t;
+
+    enum {
+        CMD_NOP,
+        CMD_READ,
+        CMD_WRITE,
+        CMD_LOCKED_READ,
+        CMD_STORE_COND = CMD_NOP,
+    };
+
+    static const unsigned int _err_mask = (1<<rerror_size_ter)-1;
+    typedef enum {
+        ERR_NORMAL = 0 & _err_mask,
+        ERR_GENERAL_DATA_ERROR = 1 & _err_mask,
+        ERR_BAD_DATA = 5 & _err_mask,
+        ERR_ABORT_DISCONNECT = 7 & _err_mask,
+    } vci_error_e;
+
+    enum {
+        STORE_COND_ATOMIC = 0,
+        STORE_COND_NOT_ATOMIC = 1,
+    };
+
+    static std::string string( const std::string &name = "" )
+    {
+        std::string vp = VciParamsString(B,K,N,E,Q,F,S,P,T,W);
+        if ( name == "" )
+            return vp;
+        return name+'<'+vp+'>';
+    }
+
+    static inline fast_data_t be2mask( fast_data_t be )
+    {
+        fast_data_t ret = (be * 0x0100040010004000ULL | be * 0x0002000800200080ULL) & 0x8080808080808080ULL;
+        ret |= ret >> 1; ret |= ret >> 2; ret |= ret >> 4;
+        return ret;
+    }
+};  // end class VciParamsTer
 
 }}
 
