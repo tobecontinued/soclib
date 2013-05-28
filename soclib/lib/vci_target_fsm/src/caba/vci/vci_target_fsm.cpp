546eb0b79a1020f06406f0d0eaef61755f9e083a
alain <alain@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2013-05-28 10:39:04 +0000

    Cosmetic
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@2338 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/lib/vci_target_fsm/src/caba/vci/vci_target_fsm.cpp
+++ soclib/lib/vci_target_fsm/src/caba/vci/vci_target_fsm.cpp
@@ -38,15 +38,17 @@ namespace caba {
 #define tmpl(x) template<typename vci_param,bool default_target,bool support_llsc> \
     x VciTargetFsm<vci_param,default_target,support_llsc>
 
+/////////////////////////
 tmpl(/**/)::VciTargetFsm(
-    soclib::caba::VciTarget<vci_param> &vci,
-    const std::list<soclib::common::Segment> &seglist )
+    soclib::caba::VciTarget<vci_param>           &vci,
+    const std::list<soclib::common::Segment>     &seglist )
     : p_vci(vci),
       m_atomic(support_llsc ? (1<<vci_param::S) : 0),
       m_segments(seglist.begin(), seglist.end())
 {
 }
 
+///////////////////////////
 tmpl(void)::_on_read_write(
     soclib::caba::BaseModule *owner_module,
     wrapper_read_t read_func,
@@ -57,6 +59,7 @@ tmpl(void)::_on_read_write(
     m_on_write_f = write_func;
 }
 
+///////////////////////////
 tmpl(/**/)::~VciTargetFsm()
 {
 }
@@ -68,6 +71,7 @@ tmpl(void)::reset()
     m_send_rsp = false;
 }
 
+////////////////////////
 tmpl(void)::handle_one()
 {
     /*
@@ -77,250 +81,214 @@ tmpl(void)::handle_one()
      */
     bool reached = false;
     std::vector<soclib::common::Segment>::const_iterator seg = m_segments.begin();
-    size_t seg_no=0;
-    addr_t address = m_current_cmd.addr;
-    addr_t offset = 0;
+    size_t seg_no       = 0;
+    addr_t address      = m_current_cmd.addr;
+    addr_t offset       = 0;
     m_current_cmd.error = 0;
-    while ( seg != m_segments.end() && !reached ) {
-        if ( seg->contains(address) ) {
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " segment " << *seg << " contains " << address << std::endl;
-#endif
+    while ( seg != m_segments.end() && !reached ) 
+    {
+        if ( seg->contains(address) ) 
+        {
             reached = true;
             offset = address - seg->baseAddress();
-        } else {
+        } 
+        else 
+        {
             ++seg_no;
             ++seg;
         }
     }
 
-    if ( !reached ) {
-        if ( default_target ) {
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " ERROR: address " << address << " not for me" << std::endl;
-#endif
+    if ( !reached ) 
+    {
+        if ( default_target ) 
+        {
             m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
-        } else {
+        } 
+        else 
+        {
             assert(0&&"Receiving wrong req and not default target");
         }
         return;
     }
 
-#ifdef SOCLIB_MODULE_DEBUG
-    std::cout
-        << name()
-        << " Handling command " << m_current_cmd.cmd
-        << " @ " << m_current_cmd.addr
-        << " off " << offset << " of " << *seg
-        << " be: " << m_current_cmd.be
-        << " wdata: " << m_current_cmd.wdata
-        << std::endl;
-#endif
     switch (m_current_cmd.cmd)
     {
-    case vci_param::CMD_WRITE:
-        if ( support_llsc )
+        case vci_param::CMD_WRITE:
+            if ( support_llsc ) m_atomic.accessDone( address );
+            m_current_cmd.rdata = 0;
+            if ( ! (m_owner->*m_on_write_f)( seg_no, 
+                                             offset, 
+                                             m_current_cmd.wdata, 
+                                             m_current_cmd.be ) ) 
+            {
+                m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
+            } 
+        break;
+        case vci_param::CMD_STORE_COND:
+            if (!support_llsc) 
+            {
+                m_current_cmd.rdata = vci_param::STORE_COND_NOT_ATOMIC;
+                m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
+                break;
+            }
+            if ( ! m_atomic.isAtomic( address, m_current_cmd.srcid ) ) 
+            {
+                m_current_cmd.rdata = vci_param::STORE_COND_NOT_ATOMIC;
+                break;
+            }
+            m_current_cmd.rdata = vci_param::STORE_COND_ATOMIC;
+
             m_atomic.accessDone( address );
-        m_current_cmd.rdata = 0;
-        if ( ! (m_owner->*m_on_write_f)(seg_no, offset, m_current_cmd.wdata, m_current_cmd.be)) {
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " ERROR: write error" << std::endl;
-#endif
-            m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
-        } else {
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " write ok" << std::endl;
-#endif
-        }
+            if ( ! (m_owner->*m_on_write_f)( seg_no, 
+                                             offset, 
+                                             m_current_cmd.wdata, 
+                                             m_current_cmd.be ) ) 
+            {
+                m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
+            }
         break;
-
-    case vci_param::CMD_STORE_COND:
-        if (!support_llsc) {
-            m_current_cmd.rdata = vci_param::STORE_COND_NOT_ATOMIC;
-            m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " ERROR: SC on a non-SC-supporting target" << std::endl;
-#endif
-            break;
-        }
-        if ( ! m_atomic.isAtomic( address, m_current_cmd.srcid ) ) {
-            m_current_cmd.rdata = vci_param::STORE_COND_NOT_ATOMIC;
-            break;
-        }
-        m_current_cmd.rdata = vci_param::STORE_COND_ATOMIC;
-
-        m_atomic.accessDone( address );
-        if ( ! (m_owner->*m_on_write_f)(seg_no, offset, m_current_cmd.wdata, m_current_cmd.be)) {
-#ifdef SOCLIB_MODULE_DEBUG
-        std::cout << name() << " ERROR: write error" << std::endl;
-#endif
-            m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
-        } else {
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " SC ok" << std::endl;
-#endif
-        }
-        break;
-
-    case vci_param::CMD_LOCKED_READ:
-        if (!support_llsc) {
-            m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
-            m_current_cmd.rdata = 0x1bad1bad;
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " ERROR: LL on a non-LL-supporting target @ "
-                      << std::hex << m_current_cmd.addr << std::endl;
-#endif
-            break;
-        }
-        m_atomic.doLoadLinked( address, m_current_cmd.srcid );
-    case vci_param::CMD_READ:
-        if ( ! (m_owner->*m_on_read_f)(seg_no, offset, m_current_cmd.rdata)) {
-#ifdef SOCLIB_MODULE_DEBUG
-        std::cout << name() << " ERROR: read error" << std::endl;
-#endif
-            m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
-            m_current_cmd.rdata = 0x0bad0bad;
-        } else {
-#ifdef SOCLIB_MODULE_DEBUG
-            std::cout << name() << " read ok, rdata: " << m_current_cmd.rdata << std::endl;
-#endif
-        }
+        case vci_param::CMD_LOCKED_READ:
+            if (!support_llsc) 
+            {
+                m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
+                m_current_cmd.rdata = 0x1bad1bad;
+                break;
+            }
+            m_atomic.doLoadLinked( address, m_current_cmd.srcid );
+        case vci_param::CMD_READ:
+            if ( ! (m_owner->*m_on_read_f)( seg_no, 
+                                            offset, 
+                                            m_current_cmd.rdata)) 
+            {
+                m_current_cmd.error = vci_param::ERR_GENERAL_DATA_ERROR;
+                m_current_cmd.rdata = 0x0bad0bad;
+            } 
         break;
     }
-}
+} // end handle_one()
 
+////////////////////////
 tmpl(void)::transition()
 {
     using soclib::common::ctz;
 
-    if ( p_vci.peerAccepted() ) {
-#ifdef SOCLIB_MODULE_DEBUG
-        std::cout
-            << name() << " peer accepted response" << std::endl
-            << " reop: " << p_vci.reop.read() << std::endl
-            << " rdata: " << p_vci.rdata.read() << std::endl
-            << " rerror: " << p_vci.rerror.read() << std::endl
-            << " rsrcid: " << p_vci.rsrcid.read() << std::endl
-            << " rtrdid: " << p_vci.rtrdid.read() << std::endl
-            << " rpktid: " << p_vci.rpktid.read() << std::endl
-            << std::endl;
-#endif
+    if ( p_vci.peerAccepted() ) 
+    {
         m_send_rsp = false;
     }
 
-#ifdef SOCLIB_MODULE_DEBUG
-    static const char *const modes[] = {
-        "IDLE", "INOUT_QUERY", "SIZED_READ",
-        "SIZED_WRITE", "SIZED_READ_FLUSH_CMD", "FLUSH_CMD" };
-    std::cout
-        << name()
-        << " send_rsp: " << m_send_rsp
-        << " " << modes[m_mode]
-        << std::endl;
-    if ( p_vci.iAccepted() ) {
-        VciCmdBuffer<vci_param> buf;
-        buf.readFrom( p_vci );
-        std::cout
-            << name()
-            << " read command: " << buf
-            << std::endl;
-    }
-#endif
+    switch ( m_mode ) 
+    {
+        case MODE_IDLE:
+            if ( p_vci.iAccepted() ) 
+            {
+                m_current_cmd.addr = p_vci.address.read() & ~(vci_param::B-1);
+                m_current_cmd.cmd = p_vci.cmd.read();
+                m_current_cmd.be = p_vci.be.read();
+                m_current_cmd.srcid = p_vci.srcid.read();
+                m_current_cmd.trdid = p_vci.trdid.read();
+                m_current_cmd.pktid = p_vci.pktid.read();
+                m_current_cmd.wdata = p_vci.wdata.read();
+                m_current_cmd.eop = p_vci.eop.read();
+                m_current_cmd.plen = p_vci.plen.read();
 
-    switch ( m_mode ) {
-    case MODE_IDLE:
-        if ( p_vci.iAccepted() ) {
-            m_current_cmd.addr = p_vci.address.read() & ~(vci_param::B-1);
-            m_current_cmd.cmd = p_vci.cmd.read();
-            m_current_cmd.be = p_vci.be.read();
-            m_current_cmd.srcid = p_vci.srcid.read();
-            m_current_cmd.trdid = p_vci.trdid.read();
-            m_current_cmd.pktid = p_vci.pktid.read();
-            m_current_cmd.wdata = p_vci.wdata.read();
-            m_current_cmd.eop = p_vci.eop.read();
-            m_current_cmd.plen = p_vci.plen.read();
-
-            if ( m_current_cmd.plen ) {
-                switch ( m_current_cmd.cmd ) {
-                case vci_param::CMD_READ:
-                    if ( m_current_cmd.eop ) {
-                        m_mode = MODE_SIZED_READ;
-                    } else {
-                        m_mode = MODE_SIZED_READ_FLUSH_CMD;
-                        m_current_cmd.base_addr = m_current_cmd.addr;
-                        m_cmd_word = 0;
+                if ( m_current_cmd.plen ) 
+                {
+                    switch ( m_current_cmd.cmd ) 
+                    {
+                        case vci_param::CMD_READ:
+                            if ( m_current_cmd.eop ) 
+                            {
+                                m_mode = MODE_SIZED_READ;
+                            }
+                            else
+                            {
+                                m_mode = MODE_SIZED_READ_FLUSH_CMD;
+                                m_current_cmd.base_addr = m_current_cmd.addr;
+                                m_cmd_word = 0;
+                            }
+                            handle_one();
+                            m_send_rsp = true;
+                            m_cells_to_go = ( m_current_cmd.plen + 
+                                              ctz(m_current_cmd.be) +
+                                              vci_param::B-1 ) / vci_param::B;
+                            m_current_cmd.eop = m_cells_to_go == 1;
+                        break;
+                        case vci_param::CMD_WRITE:
+                            if ( m_current_cmd.eop ) 
+                            {
+                                m_mode = MODE_INOUT_QUERY;
+                                handle_one();
+                                m_send_rsp = true;
+                                assert( m_current_cmd.plen <= vci_param::B );
+                            } 
+                            else 
+                            {
+                                m_mode = MODE_SIZED_WRITE;
+                                handle_one();
+                                m_current_cmd.base_addr = m_current_cmd.addr;
+                                m_cells_to_go = ( m_current_cmd.plen + 
+                                                  ctz(m_current_cmd.be) +
+                                                  vci_param::B-1 ) / vci_param::B;
+                            }
+                        break;
+                        default:
+                            m_mode = MODE_INOUT_QUERY;
+                            handle_one();
+                            m_send_rsp = true;
+                            m_cells_to_go = 1;
+                            assert( m_current_cmd.eop && 
+                            "LL-SC command with no EOP ? multi-word LL/SC ???");
                     }
-                    handle_one();
-                    m_send_rsp = true;
-                    m_cells_to_go = (m_current_cmd.plen+ctz(m_current_cmd.be)+vci_param::B-1)/vci_param::B;
-                    m_current_cmd.eop = m_cells_to_go == 1;
-                    break;
-                case vci_param::CMD_WRITE:
-                    if ( m_current_cmd.eop ) {
-                        m_mode = MODE_INOUT_QUERY;
-                        handle_one();
-                        m_send_rsp = true;
-                        assert( m_current_cmd.plen <= vci_param::B );
-                    } else {
-                        m_mode = MODE_SIZED_WRITE;
-                        handle_one();
-                        m_current_cmd.base_addr = m_current_cmd.addr;
-                        m_cells_to_go = (m_current_cmd.plen+ctz(m_current_cmd.be)+vci_param::B-1)/vci_param::B;
-                    }
-                    break;
-                default:
+                } 
+                else 
+                {
                     m_mode = MODE_INOUT_QUERY;
                     handle_one();
                     m_send_rsp = true;
                     m_cells_to_go = 1;
-                    assert( m_current_cmd.eop && "LL-SC command with no EOP ? multi-word LL/SC ???");
                 }
-            } else {
-                    m_mode = MODE_INOUT_QUERY;
-                    handle_one();
-                    m_send_rsp = true;
-                    m_cells_to_go = 1;
             }
-        }
         break;
-    case MODE_INOUT_QUERY:
-        if ( m_send_rsp )
-            break;
-
-        m_mode = MODE_IDLE;
+        case MODE_INOUT_QUERY:
+            if ( m_send_rsp ) break;
+            m_mode = MODE_IDLE;
         break;
-
-    case MODE_SIZED_WRITE:
-        if ( p_vci.iAccepted() ) {
-            m_cells_to_go--;
-            m_current_cmd.addr += vci_param::B;
-            if ( m_current_cmd.addr != p_vci.address.read() ) {
-                std::cerr
+        case MODE_SIZED_WRITE:
+            if ( p_vci.iAccepted() ) 
+            {
+                m_cells_to_go--;
+                m_current_cmd.addr += vci_param::B;
+                if ( m_current_cmd.addr != p_vci.address.read() ) 
+                {
+                    std::cerr
                     << name()
                     << "Uncontiguous addresses in write command," << std::hex
                     << " expected " << m_current_cmd.addr
                     << " and got " << p_vci.address.read()
                     << " base_address " << m_current_cmd.base_addr
                     << std::endl;
-                VciCmdBuffer<vci_param> buf;
-                buf.readFrom( p_vci );
-                std::cout
+                    VciCmdBuffer<vci_param> buf;
+                    buf.readFrom( p_vci );
+                    std::cout
                     << name()
                     << " current command: " << buf
                     << std::endl;
-                abort();
-            }
-            m_current_cmd.wdata = p_vci.wdata.read();
-            m_current_cmd.be = p_vci.be.read();
-            m_current_cmd.eop = p_vci.eop.read();
-            handle_one();
+                    abort();
+                }
+                m_current_cmd.wdata = p_vci.wdata.read();
+                m_current_cmd.be = p_vci.be.read();
+                m_current_cmd.eop = p_vci.eop.read();
+                handle_one();
 
-            if ( m_current_cmd.eop ) {
-                m_mode = MODE_IDLE;
-                assert( m_cells_to_go == 1 );
-                m_send_rsp = true;
+                if ( m_current_cmd.eop ) 
+                {
+                    m_mode = MODE_IDLE;
+                    assert( m_cells_to_go == 1 );
+                    m_send_rsp = true;
+                }
             }
-        }
         break;
 
     case MODE_SIZED_READ_FLUSH_CMD:
@@ -358,29 +326,33 @@ tmpl(void)::transition()
     }
 }
 
+//////////////////////
 tmpl(void)::genMoore()
 {
-    switch ( m_mode ) {
-    case MODE_IDLE:
+    switch ( m_mode ) 
+    {
+        case MODE_IDLE:
         p_vci.cmdack = ! m_send_rsp;
         break;
-    case MODE_SIZED_READ_FLUSH_CMD:
-    case MODE_FLUSH_CMD:
+        case MODE_SIZED_READ_FLUSH_CMD:
+        case MODE_FLUSH_CMD:
         p_vci.cmdack = true;
         break;
-    case MODE_INOUT_QUERY:
-    case MODE_SIZED_READ:
+        case MODE_INOUT_QUERY:
+        case MODE_SIZED_READ:
         p_vci.cmdack = false;
         break;
-    case MODE_SIZED_WRITE:
+        case MODE_SIZED_WRITE:
         p_vci.cmdack = ! m_current_cmd.eop;
         break;
     }
 
 	p_vci.rspval = m_send_rsp;
-    p_vci.rspSetIds( m_current_cmd.srcid, m_current_cmd.trdid, m_current_cmd.pktid );
-    p_vci.reop = m_current_cmd.eop;
-    p_vci.rdata = m_current_cmd.rdata;
+    p_vci.rsrcid = m_current_cmd.srcid; 
+    p_vci.rtrdid = m_current_cmd.trdid; 
+    p_vci.rpktid = m_current_cmd.pktid; 
+    p_vci.reop   = m_current_cmd.eop;
+    p_vci.rdata  = m_current_cmd.rdata;
     p_vci.rerror = m_current_cmd.error;
 }
 
