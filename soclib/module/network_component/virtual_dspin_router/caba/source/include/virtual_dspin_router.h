29ec3d897f7323b868cac009c5e68d3c89f53f70
joannou <joannou@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2013-07-23 12:35:37 +0000

    In virtual_dspin_router : Made the number of virtual channels generic (new constructor argument)
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@2410 2cf4c1f0-a32b-0410-9068-d80f8be93c97

--- soclib/module/network_component/virtual_dspin_router/caba/source/include/virtual_dspin_router.h
+++ soclib/module/network_component/virtual_dspin_router/caba/source/include/virtual_dspin_router.h
@@ -19,8 +19,9 @@
  * 
  * SOCLIB_LGPL_HEADER_END
  *
- * Authors  : alain.greiner@lip6.fr noe.girand@polytechnique.org
- * Date     : july 2010
+ * Authors      : alain.greiner@lip6.fr noe.girand@polytechnique.org
+ * Date         : july 2010
+ * Maintainers  : alexandre.joannou@lip6.fr
  * Copyright: UPMC - LIP6
  */
 
@@ -77,8 +78,8 @@ enum{  // port indexing
 	// ports
 	sc_core::sc_in<bool>             	    p_clk;
 	sc_core::sc_in<bool>             	    p_resetn;
-	soclib::caba::DspinOutput<flit_width>	**p_out;
-	soclib::caba::DspinInput<flit_width>	**p_in;
+	soclib::caba::DspinOutput<flit_width>*  p_out[5];
+	soclib::caba::DspinInput<flit_width>*   p_in[5];
 
 	// constructor 
 	VirtualDspinRouter( sc_module_name  insname,
@@ -86,6 +87,7 @@ enum{  // port indexing
                         int	            y,                  // y coordinate in the mesh 
                         int	            x_width,            // number of bits for x field
                         int	            y_width,            // number of bits for y field
+                        size_t          nb_chan,            // number of virtual channels
                         int	            in_fifo_depth,		// input fifo depth
                         int	            out_fifo_depth );	// output fifo depth
 
@@ -93,8 +95,8 @@ enum{  // port indexing
 	~VirtualDspinRouter();
 
 	// public methods
-        void	print_trace(int channel = 0);
-        void	debug_trace(int channel = 0);
+        void	print_trace(size_t channel = 0);
+        void	debug_trace(size_t channel = 0);
 
 private:
 
@@ -107,27 +109,28 @@ private:
     
 
 	// input port registers & fifos
-	sc_core::sc_signal<bool>              r_tdm[5];              // Time Multiplexing
-	sc_core::sc_signal<int>			      r_input_fsm[2][5];	 // FSM state
-	internal_flit_t                       r_buf[2][5];           // fifo extension
+	sc_core::sc_signal<bool>*                   r_tdm[5];           // Time Multiplexing
+	sc_core::sc_signal<int>*                    r_input_fsm[5];	    // FSM state
+	internal_flit_t*                            r_buf[5];           // fifo extension
 
 
 	// output port registers & fifos
-	sc_core::sc_signal<int>              r_output_index[2][5];  // allocated input  
-	sc_core::sc_signal<bool>			 r_output_alloc[2][5];	// allocation status 
+	sc_core::sc_signal<int>*                    r_output_index[5];  // allocated input  
+	sc_core::sc_signal<bool>*                   r_output_alloc[5];  // allocation status 
 
-	soclib::caba::GenericFifo<internal_flit_t>**   in_fifo;     // input fifos
-	soclib::caba::GenericFifo<internal_flit_t>**   out_fifo;    // output fifos
+	soclib::caba::GenericFifo<internal_flit_t>* in_fifo[5];         // input fifos
+	soclib::caba::GenericFifo<internal_flit_t>* out_fifo[5];        // output fifos
 
 	// structural variables
-	int	m_local_x;					// router x coordinate
-	int	m_local_y;					// router y coordinate
-    int	m_x_width;	 				// number of bits for x field
-    int	m_y_width;					// number of bits for y field
-    int	m_x_shift;					// number of bits to shift for x field
-    int	m_x_mask;					// number of bits to mask for x field
-    int	m_y_shift;					// number of bits to shift for y field
-    int	m_y_mask;					// number of bits to mask for y field
+	int	m_local_x;                          // router x coordinate
+	int	m_local_y;                          // router y coordinate
+    int	m_x_width;                          // number of bits for x field
+    int	m_y_width;                          // number of bits for y field
+    int	m_x_shift;                          // number of bits to shift for x field
+    int	m_x_mask;                           // number of bits to mask for x field
+    int	m_y_shift;                          // number of bits to shift for y field
+    int	m_y_mask;                           // number of bits to mask for y field
+    size_t	m_nb_chan;                      // number of bits to mask for y field
 
 	// methods 
 	void transition();
