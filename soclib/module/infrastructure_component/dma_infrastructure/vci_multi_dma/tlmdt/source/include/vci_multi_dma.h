9ded1c41a5f818f61c26c21baeef749e98ce4b79
alain <alain@2cf4c1f0-a32b-0410-9068-d80f8be93c97>
2014-01-06 11:06:25 +0000

    Introducing a TLMDT model for vci_multi_dma.
    git-svn-id: https://www.soclib.fr/svn/trunk/soclib@2487 2cf4c1f0-a32b-0410-9068-d80f8be93c97

---
+++ soclib/module/infrastructure_component/dma_infrastructure/vci_multi_dma/tlmdt/source/include/vci_multi_dma.h
@@ -0,0 +1,211 @@
+/* -*- c++ -*-
+ *
+ * SOCLIB_LGPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU LGPLv2.1.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * SOCLIB_LGPL_HEADER_END
+ *
+ * Copyright (c) UPMC, Lip6, Asim
+ *         Alain Greiner  <alain.greiner@lip6.fr> 2011
+ *
+ * Maintainers: alain
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Implementation Note
+// This component is  a multi-channels DMA controller, acting both
+// as a VCI initiator and as a VCI target.
+// The number of channels is variable, but cannot be larger than 8.
+//
+// The target FSM is modeled by a purely reactive interface function.
+//
+// The initiator FSMs corresponding to the various parallel channels are 
+// modeled by one single sc_thread, implementing all the initiator FSMs,
+// but each channel has a private set of state registers.
+//
+// This component has a local time, that is updated depending on the
+// DMA component global state:
+// - when there is no channel activated, it requires periodical NULL 
+//   messages from the interconnect, to be updated by  message received 
+//   on the VCI target port (VCI commands or NULL messages).
+// - when at least one channel is activated, the local time is updated 
+//   by the local thread.
+//
+// Each initiator FSM can be in 6 states :
+// - IDLE        : not running : waiting a VCI command on the target port
+// - READ        : running     : performing a VCI read transaction
+// - WRITE       : running     : performing a VCI write transaction
+// - SUCCESS     : not running : waiting a VCI command on the target port
+// - ERROR_READ  : not running : waiting a VCI command on the target port
+// - ERROR_WRITE : not running : waiting a VCI command on the target port
+//
+// When the DMA is in a running state (READ or WRITE), the configuration
+// commands are ignored, and the registers are not modified.
+// In these running states, it makes continuously read/write transactions.
+// there is no need to send null messages.,
+// When the DMA is not in a running state, it send null messages.
+// The IRQ flip-flop is implemented as an unsigned char.
+// The IRQ value is periodically transmitted on the IRQ port (each time there is
+// a VCI command or a NULL message) to allow the ICU to implement
+// the PDES time filtering.
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef SOCLIB_VCI_MULTI_DMA_H
+#define SOCLIB_VCI_MULTI_DMA_H
+
+#include <stdint.h>
+#include <systemc>
+#include <tlmdt>
+#include "mapping_table.h"
+
+namespace soclib {
+namespace tlmdt {
+
+using namespace sc_core;
+
+template<typename vci_param>
+class VciMultiDma
+  : public sc_core::sc_module
+  , virtual public tlm::tlm_bw_transport_if<tlm::tlm_base_protocol_types> 
+  , virtual public tlm::tlm_fw_transport_if<tlm::tlm_base_protocol_types> 
+{
+    typedef typename vci_param::addr_t addr_t;
+    typedef typename vci_param::data_t data_t;
+
+private:
+
+    // structural constants
+    const uint32_t 		m_srcid;                  // DMA SRCID
+    const size_t        m_max_burst_length;	      // local buffer length (bytes)
+    const size_t        m_channels;               // number of parallel channels
+
+    soclib::common::Segment 	m_segment;        // segment associated to DMA
+
+    // registers
+    int                 m_state[8];               // channel state
+    data_t              m_source[8];              // source buffer address
+    data_t              m_destination[8];         // destination buffer address
+    data_t              m_length[8];              // tranfer length (bytes)
+    bool                m_irq_disabled[8];        // no IRQ when true
+    bool                m_stop[8];                // running when false
+    uint8_t             m_irq_value[8];	          // IRQ current value
+    uint8_t*            m_vci_data_buf[8];        // local data buffer pointer  
+
+    pdes_local_time*	m_pdes_local_time;        // local time pointer
+
+    sc_core::sc_event   m_rsp_received;		      // event to wake-up the thread
+
+    // VCI TRANSACTION
+    tlm::tlm_generic_payload    m_vci_payload;
+    tlm::tlm_phase              m_vci_phase;
+    sc_core::sc_time            m_vci_time;
+    soclib_payload_extension	m_vci_extension;
+    
+    // NULL MESSAGE
+    tlm::tlm_generic_payload    m_null_payload;
+    tlm::tlm_phase              m_null_phase;
+    sc_core::sc_time            m_null_time;
+    soclib_payload_extension    m_null_extension;
+    
+    // IRQ[channe] TRANSACTIONS
+    tlm::tlm_generic_payload    m_irq_payload[8];
+    tlm::tlm_phase              m_irq_phase[8];
+    sc_core::sc_time            m_irq_time[8];
+
+public:
+
+    enum init_state_e 
+    {
+        STATE_IDLE,
+        STATE_SUCCESS,
+        STATE_ERROR_READ,
+        STATE_ERROR_WRITE,
+        STATE_READ,
+        STATE_WRITE,
+    };
+
+    // Functions
+    void execLoop();
+    void send_null();
+    void send_write();
+    void send_read();
+
+    // Interface function to receive response on the VCI initiator port
+    tlm::tlm_sync_enum 	nb_transport_bw ( tlm::tlm_generic_payload   &payload,
+                                          tlm::tlm_phase             &phase,  
+                                          sc_core::sc_time           &time);   
+    
+    // Interface Function to receive response on the IRQ port
+    tlm::tlm_sync_enum 	irq_nb_transport_bw ( tlm::tlm_generic_payload  &payload, 
+                                              tlm::tlm_phase            &phase,  
+                                              sc_core::sc_time          &time);  
+
+    // Interface function to receive command on the VCI target port
+    tlm::tlm_sync_enum 	nb_transport_fw ( tlm::tlm_generic_payload &payload,  
+                                          tlm::tlm_phase           &phase,      
+                                          sc_core::sc_time         &time);        
+/*    
+    // Not implemented but mandatory
+    void b_transport ( tlm::tlm_generic_payload &payload, 
+                       sc_core::sc_time         &time); 
+    
+    bool get_direct_mem_ptr ( tlm::tlm_generic_payload &payload,  
+                              tlm::tlm_dmi             &dmi_data);  
+    
+    unsigned int transport_dbg ( tlm::tlm_generic_payload &payload); 
+    
+    void invalidate_direct_mem_ptr ( sc_dt::uint64 start_range,  
+                                     sc_dt::uint64 end_range); 
+*/
+
+protected:
+
+    SC_HAS_PROCESS(VciMultiDma);
+    
+public:
+
+    // ports
+    tlm::tlm_initiator_socket<32, tlm::tlm_base_protocol_types>     p_vci_initiator; 
+
+    tlm::tlm_target_socket<32,tlm::tlm_base_protocol_types>         p_vci_target;
+
+    std::vector<tlm_utils::simple_initiator_socket_tagged
+    <VciMultiDma,32,tlm::tlm_base_protocol_types> *>                p_irq; 
+
+
+    VciDma( sc_module_name                     name,
+            const soclib::common::MappingTable &mt,
+            const soclib::common::IntTab       &srcid,
+            const soclib::common::IntTab       &tgtid,
+            const size_t                       max_burst_length,
+            const size_t                       channels );
+};
+
+}}
+
+#endif /* SOCLIB_VCI_DMA_H */
+
+// Local Variables:
+// tab-width: 4
+// c-basic-offset: 4
+// c-file-offsets:((innamespace . 0)(inline-open . 0))
+// indent-tabs-mode: nil
+// End:
+
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4
+
---
+++ soclib/module/infrastructure_component/dma_infrastructure/vci_multi_dma/tlmdt/source/src/vci_multi_dma.cpp
@@ -0,0 +1,504 @@
+/* -*- c++ -*-
+ *
+ * SOCLIB_LGPL_HEADER_BEGIN
+ * 
+ * This file is part of SoCLib, GNU LGPLv2.1.
+ * 
+ * SoCLib is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 of the License.
+ * 
+ * SoCLib is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SoCLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ * 
+ * SOCLIB_LGPL_HEADER_END
+ *
+ * Copyright (c) UPMC, Lip6, Asim
+ *         Alain Greiner <alain.greiner@lip6.fr>, 2011
+ */
+
+#include <stdint.h>
+#include <iostream>
+#include "register.h"
+#include "../include/vci_multi_dma.h"
+#include <fcntl.h>
+#include "multi_dma.h"
+
+namespace soclib { namespace tlmdt {
+
+#define tmpl(t) template<typename vci_param> t VciDma<vci_param>
+
+/////////////////////////////////////////
+tmpl (void)::send_write( size_t k )
+{
+    m_pdes_local_time->reset_sync();
+    
+    // send VCI command
+    m_vci_payload[k].set_address(m_destination[k] & ~3);
+    m_vci_payload.set_byte_enable_length(m_burst_length[k]);
+    m_vci_payload.set_data_length(m_burst_length[k]);
+    m_vci_extension[k].set_write();
+    m_vci_time[k] = m_pdes_local_time->get();
+
+    p_vci_initiator->nb_transport_fw( m_vci_payload[k], 
+                                      m_vci_phase[k], 
+                                      m_vci_time[k] );
+#ifdef SOCLIB_MODULE_DEBUG
+std::cout <<  "[" <<name() << "] time = " << std::dec << m_vci_time[k].value() 
+          << std::hex << " WRITE COMMAND / address = " << m_destination[k]
+          << " length = " << std::dec << m_burst_length[k] << std::endl;
+#endif
+}
+
+//////////////////////////////////
+tmpl (void)::send_read( size_t k )
+{
+    m_pdes_local_time->reset_sync();
+
+    // send VCI read command
+    m_vci_payload[k].set_address(m_source[k] & ~3);
+    m_vci_payload[k].set_byte_enable_length(m_burst_length[k]);
+    m_vci_payload[k].set_data_length(m_burst_length[k]);
+    m_vci_extension[k].set_read();
+    m_vci_time[k] = m_pdes_local_time->get();
+
+    p_vci_initiator->nb_transport_fw( m_vci_payload[k], 
+                                      m_vci_phase[k], 
+                                      m_vci_time[k] );
+#ifdef SOCLIB_MODULE_DEBUG
+std::cout <<  "[" <<name() << "] time = " << std::dec << m_vci_time[k].value()
+          << std::hex << " READ  COMMAND / address = " << m_source[k] 
+          << " length = " << std::dec << m_burst_length[k] << std::endl;
+#endif
+}
+
+////////////////////////
+tmpl (void)::send_null()
+{
+    m_pdes_local_time->reset_sync();
+
+    // send NULL message
+    m_null_time = m_pdes_local_time->get();
+    p_vci_initiator->nb_transport_fw(m_null_payload, m_null_phase, m_null_time);
+
+    // send IRQ
+    m_irq_time = m_pdes_local_time->get();
+    p_irq->nb_transport_fw(m_irq_payload, m_irq_phase, m_irq_time);
+
+#ifdef SOCLIB_MODULE_DEBUG
+std::cout <<  "[" <<name() << "] time = " << std::dec << m_null_time.value()
+          << " NULL MESSAGE / IRQ value = " << (int)m_irq_value << std::endl;
+#endif
+}
+
+////////////////////////////////////////////
+// thread associated to the initiator FSMs
+////////////////////////////////////////////
+tmpl (void)::execLoop ()
+{
+    while(true)
+    {
+        // deschedule if all channels are IDLE
+        bool all_idle = true;
+        for ( size_t k = 0 ; k < m_channels ; k++ )
+        {
+            if ( m_state[k] != STATE_IDLE ) all_idle = false;
+        }
+        if ( all_idle ) wait( m_channel_activated );
+
+        // enters the loop on all channels
+        for ( size_t k = 0 ; k < m_channels ; k++ )
+        {
+            switch ( m_state[k] ) {
+            ////////////////
+            case STATE_IDLE:
+            {
+                if ( !m_stop[k] ) m_state[k] = STATE_READ_CMD;
+                break;
+            } 
+            ////////////////////
+            case STATE_READ_CMD:
+            {
+                if ( m_length[k] >= m_max_burst ) m_burst_length[k] = m_max_burst;
+                else                              m_burst_length[k] = m_length[k];
+                send_read( channel );
+                m_state[k] = STATE_READ_RSP;
+                break;
+            }
+            ////////////////////
+            case STATE_READ_RSP:
+            {
+                if ( m_rsp_received[k] ) 
+                {
+                    m_rsp_received[k] = false;
+
+                    if ( m_stop[k] )
+                    {
+                        m_state[k] = STATE_IDLE;
+                    }
+                    else if( !m_vci_payload.is_response_error() )
+                    {
+                        m_state[k] = STATE_WRITE_CMD;
+                    }
+                    else
+                    {
+                        m_state[k] = STATE_ERROR_WRITE;
+                        if ( !m_irq_disabled[k] ) send_irq( channel );
+                    }
+                }
+                break;
+            }
+            /////////////////////
+            case STATE_WRITE_CMD:
+            {
+                send_write( channel );
+                m_state[k] = STATE_WRITE_RSP;
+                break;
+            }
+            /////////////////////
+            case STATE_WRITE_RSP:
+            {
+                if ( m_rsp_received[k] )
+                {
+                    m_rsp_received[k] = false;
+
+                    if ( m_stop[k] ) 		
+                    {
+                        m_state[k] = STATE_IDLE;
+                    }
+                    else if ( m_vci_payload.is_response_error() )
+                    {
+                        m_state[k] = STATE_ERROR_WRITE;
+                        if ( !m_irq_disabled[k] ) send_irq( channel );
+                    }
+                    else if ( m_length[k] == 0 )
+                    {
+                        m_state[k] = STATE_SUCCESS;
+                        if ( !m_irq_disabled[k] ) send_irq( channel );
+                    }
+                    else
+                    {
+                        m_length[k]            = m_length[k]      - m_burst_length[k];
+                        m_source[k]            = m_source[k]      + m_burst_length[k];
+                        m_destination[k]       = m_destination[k] + m_burst_length[k];
+                        m_state[k]             = STATE_READ_CMD;
+                    }
+                }
+                break;
+            }
+            //////////////////////
+            case STATE_ERROR_READ:
+            case STATE_ERROR_WRITE:
+            case STATE_SUCCESS:
+            {
+                if ( m_stop[k] ) 
+                {
+                    m_state = STATE_IDLE;
+                    m_irq_value[k] = 0x00;
+                    send_irq( k ); 
+                }
+                else if ( m_pdes_local_time->need_sync() )
+                {
+                    send_null();
+                }
+            } } // end switch 
+
+                else if ( m_pdes_local_time->need_sync() )
+                {
+                    send_null();
+                    wait(m_rsp_received);
+                }
+
+    } // end while
+} // end execLoop
+
+////////////////////////////////////////////////////////////////////////////////
+// Interface function executed when receiving a response on the initiator port
+////////////////////////////////////////////////////////////////////////////////
+tmpl (tlm::tlm_sync_enum)::nb_transport_bw ( tlm::tlm_generic_payload &payload, 
+                                             tlm::tlm_phase           &phase,       
+                                             sc_core::sc_time         &time)  
+{
+    // update local time and notify
+    if( time.value() > m_pdes_local_time->get().value()) m_pdes_local_time->set(time);
+    m_rsp_received.notify (sc_core::SC_ZERO_TIME);
+    return tlm::TLM_COMPLETED;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Interface function executed when receiving a response to an IRQ transaction
+/////////////////////////////////////////////////////////////////////////////////////
+tmpl (tlm::tlm_sync_enum)::irq_nb_transport_bw ( int                         id,
+                                                 tlm::tlm_generic_payload    &payload,
+                                                 tlm::tlm_phase              &phase,
+                                                 sc_core::sc_time            &time ) 
+{
+    // no action
+    return tlm::TLM_COMPLETED;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+// Interface function used when receiving a VCI command on the target port
+// As the dated transactions on the VCI target port are used to update the DMA
+// local time when all DMA channels are IDLE, this component requires periodical
+// NULL messages from the interconnect. 
+////////////////////////////////////////////////////////////////////////////////////
+tmpl(tlm::tlm_sync_enum)::nb_transport_fw ( tlm::tlm_generic_payload &payload,
+                                            tlm::tlm_phase           &phase, 
+                                            sc_core::sc_time         &time)   
+{
+    int 	cell;
+    int     reg;
+    int     channel;
+
+    soclib_payload_extension* extension_pointer;
+    payload.get_extension(extension_pointer);
+    
+    // Compute global state
+    bool all_idle = true;
+    for( size_t k = 0 ; k < m_channels ; k++ )
+    {
+        if( m_state[k] != STATE_IDLE ) all_idle = false;
+    }
+
+    // update local time if all channels IDLE 
+    if ( all_idle and  (m_pdes_local_time->get().value() < time.value()) )
+    {
+        m_pdes_local_time->set( time );
+    }
+
+    // No other action on NULL messages
+    if ( extension_pointer->is_null_message() ) 
+    {
+
+#if SOCLIB_MODULE_DEBUG
+std::cout << "[" << name() << "] time = "  << time.value() 
+          << " Receive NULL message" << std::endl;
+#endif
+        return tlm::TLM_COMPLETED;
+    }
+
+    // address and length checking for a VCI command
+    bool	one_flit = (payload.get_data_length() == (unsigned int)vci_param::B);
+    addr_t	address = payload.get_address();
+
+    if ( m_segment.contains(address) && one_flit )
+    {
+        cell    = (int)((address - m_segment.baseAddress()) / vci_param::B);
+        reg     = cell % DMA_SPAN;
+        channel = cell / DMA_SPAN;
+
+        // checking channel overflow
+        if ( channel < m_channels ) payload.set_response_status(tlm::TLM_OK_RESPONSE);
+        else             payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+
+        if ( extension_pointer->get_command() == VCI_READ_COMMAND )
+        {
+
+#if SOCLIB_MODULE_DEBUG
+std::cout << "[" << name() << "] time = "  << time.value() 
+          << " Receive VCI read command : address = " << std::hex << address
+          << " / channel = " << std::dec << channel
+          << " / reg = "  << reg << std::endl;
+#endif
+        
+            if      ( reg == DMA_SRC )
+                    utoa( m_source[channel], payload.get_data_ptr(), 0 );
+            else if ( reg == DMA_DST )
+                    utoa( m_destination[channel], payload.get_data_ptr(), 0 );
+            else if ( reg == DMA_LEN )
+                    utoa( m_state[channel], payload.get_data_ptr(), 0 );
+            else if ( reg == DMA_IRQ_DISABLED ) 
+                    utoa( (int)(m_irq_disabled[channel]), payload.get_data_ptr(), 0 );
+            else    payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+        } // end read
+
+        else if (extension_pointer->get_command() == VCI_WRITE_COMMAND)
+        {
+            uint32_t data = atou(payload.get_data_ptr(), 0);
+
+#if SOCLIB_MODULE_DEBUG
+std::cout << "[" << name() << "] time = "  << time.value() 
+          << " Receive VCI write command : address = " << std::hex << address
+          << " / channel = " << std::dec << channel
+          << " / reg = "  << reg 
+          << " / data = " << data << std::endl;
+#endif
+            // configuration command other than soft reset
+            // are ignored when the DMA channel is active
+            if ( reg == DMA_RESET )
+            {
+                m_stop[channel] = true;
+            }
+            else if ( m_state[channel] != STATE_IDLE )
+            {
+                if      ( reg == DMA_SRC )  m_source[channel]      = data;
+                else if ( reg == DMA_DST )  m_destination[channel] = data;
+                else if ( reg == DMA_LEN ) 
+                {
+                    m_length[channel] = data;
+                    m_stop [channel]  = false;
+                }
+                else if ( cell == DMA_IRQ_DISABLED )  m_irq_disabled[channel] = (data != 0);
+                else    payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+            }
+            else
+            {
+                std::cout << name() 
+                << " warning: receiving a new command while busy, ignored" << std::endl;
+            }
+        } // end if write
+        else // illegal command
+        {
+            payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+        }
+    } // end if legal address 
+    else // illegal address
+    {
+        payload.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+    }
+
+    // update transaction phase and time
+    phase = tlm::BEGIN_RESP;
+    if ( time.value() < m_pdes_local_time->get().value() ) time  = m_pdes_local_time->get();
+    else                                                   time  = time + UNIT_TIME;
+
+    // send response 
+    p_vci_target->nb_transport_bw(payload, phase, time);
+
+    return tlm::TLM_COMPLETED;
+}
+
+/*
+/////////////////////////////////////////////////////////////
+// Not implemented but required by interface
+/////////////////////////////////////////////////////////////
+tmpl(void)::b_transport ( tlm::tlm_generic_payload &payload, 
+                          sc_core::sc_time         &_time)
+{
+    return;
+}
+
+tmpl(bool)::get_direct_mem_ptr ( tlm::tlm_generic_payload &payload, 
+                                 tlm::tlm_dmi             &dmi_data) 
+{ 
+    return false;
+}
+    
+tmpl(unsigned int):: transport_dbg ( tlm::tlm_generic_payload &payload)
+{
+    return false;
+}
+
+tmpl(void)::invalidate_direct_mem_ptr ( sc_dt::uint64 start_range,
+                                        sc_dt::uint64 end_range) 
+{
+    return;
+}
+*/
+
+///////////////////
+//  constructor
+///////////////////
+tmpl(/**/)::VciMultiDma( sc_module_name                     name,
+                         const soclib::common::MappingTable &mt,
+                         const soclib::common::IntTab       &srcid,
+                         const soclib::common::IntTab       &tgtid,
+                         const size_t                       max_burst_length,
+                         const size_t                       channels )
+          : sc_core::sc_module(name)
+          , m_srcid(mt.indexForId(srcid))
+          , m_max_burst(max_burst_length)
+          , m_channels(channels)
+          , m_segment(mt.getSegment(tgtid))
+          , p_vci_initiator("p_vci_init") 
+          , p_vci_target("p_vci_tgt")    
+{
+    // initialize PDES local time
+    m_pdes_local_time = new pdes_local_time(100*UNIT_TIME);
+    
+    assert( (channels <= 8) && "The number of channels cannot be larger than 8");
+    assert( (max_burst_length <= 64) && "The burst length cannot be larger than 64");
+
+    // bind vci initiator port
+    p_vci_initiator(*this);                     
+    
+    // bind vci target port
+    p_vci_target(*this);                     
+    
+    // initialize channels
+    for( size_t channel ; channel < channels ; channel++ )
+    {
+        // bind IRQ[channel] ports
+        std::ostringstream name;
+        name << "p_irq_" << channel;
+        p_irq.push_back(new tlm_utils::simple_initiator_socket_tagged
+                                <VciMultiIcu,32,tlm::tlm_base_protocol_types>
+                                (name.str().c_str()));
+
+        p_irq[channel]->register_nb_transport_bw( this,
+                                                  &VciMultiIcu::irq_nb_transport_bw, 
+                                                  channel );
+
+        m_irq_out_payload[channel].set_data_ptr(&m_irq_out_value[channel]);
+        m_irq_out_phase[channel] = tlm::BEGIN_REQ;
+    
+        // create and initialize the local buffers for VCI transactions 
+        m_vci_data_buf[channel] = new unsigned char[max_burst_length];
+        m_vci_be_buf[channel]   = new unsigned char[max_burst_length];
+
+        // initialize payload, phase and extension for VCI transactions
+        m_vci_payload[channel].set_command(tlm::TLM_IGNORE_COMMAND);
+        m_vci_payload[channel].set_data_ptr(m_vci_data_buf[channel]);
+        m_vci_payload[channel].set_byte_enable_ptr(m_vci_be_buf[channel]);
+        m_vci_payload[channel].set_extension(&m_vci_extension[channel]);
+
+        m_vci_extension[channel].set_src_id(m_srcid);
+        m_vci_extension[channel].set_trd_id(channel);
+        m_vci_extension[channel].set_pkt_id(0);
+
+        m_vci_phase[channel] = tlm::BEGIN_REQ;
+
+        // initialize payload and phase for an irq message
+        m_irq_payload[channel].set_data_ptr(&m_irq_value[channel]);
+        m_irq_phase = tlm::BEGIN_REQ;
+
+        // channel state registers initialisation 
+        m_state[channel]        = STATE_IDLE;
+        m_stop[channel]         = true;
+        m_irq_disabled[channel] = false;
+        m_rsp_received[channel] = false;
+        m_irq_value[channel]    = 0;
+        for ( size_t i=0 ; i<max_burst_length ; i++) 
+        {
+            m_vci_data_buf[channel][i] = 0x0;
+            m_vci_be_buf[channel][i]   = 0xFF;
+        }
+    }
+
+    // initialize payload, phase and extension for a null message
+    m_null_payload.set_extension(&m_null_extension);
+    m_null_extension.set_null_message();
+    m_null_phase = tlm::BEGIN_REQ;
+
+    SC_THREAD(execLoop);
+}
+
+}}
+
+// Local Variables:
+// tab-width: 4
+// c-basic-offset: 4
+// cle-offsets:((innamespace . 0)(inline-open . 0))
+// indent-tabs-mode: nil
+// End:
+
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4
+
